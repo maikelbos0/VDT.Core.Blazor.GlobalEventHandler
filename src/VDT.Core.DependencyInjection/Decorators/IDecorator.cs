b40be5b7d79ea89adf72c9ebdd4748de3b21820e
Maikel Bos <maikel.bos0@gmail.com>
2021-02-22 20:53:12 +0100

    First setup of extension methods

---
+++ src/VDT.Core.DependencyInjection/Decorators/IDecorator.cs
@@ -0,0 +1,7 @@
+﻿namespace VDT.Core.DependencyInjection.Decorators {
+    public interface IDecorator {
+        void BeforeExecute() { }
+        void AfterExecute() { }
+        void OnError() { }
+    }
+}
---
+++ src/VDT.Core.DependencyInjection/Decorators/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace VDT.Core.DependencyInjection.Decorators {
+    public static class ServiceCollectionExtensions {
+        public static IServiceCollection AddScoped<TService, TImplementation, TMethodDecorator>(this IServiceCollection services)
+            where TService : class
+            where TImplementation : class, TService
+            where TMethodDecorator : IDecorator {
+
+            return services.AddScoped<TService, TImplementation>();
+        }
+
+        public static IServiceCollection AddScoped<TService, TImplementation, TMethodDecorator>(this IServiceCollection services, Func<IServiceProvider, TImplementation> implementationFactory)
+            where TService : class
+            where TImplementation : class, TService
+            where TMethodDecorator : IDecorator {
+
+            return services.AddScoped<TService, TImplementation>(implementationFactory);
+        }
+    }
+}

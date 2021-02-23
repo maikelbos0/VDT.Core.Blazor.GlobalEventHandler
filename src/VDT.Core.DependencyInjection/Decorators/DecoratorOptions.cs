2529db5378e7afa5a5c93242ffc8c372820a503e
Maikel Bos <maikel.bos0@gmail.com>
2021-02-23 21:14:12 +0100

    Basic decorator options

--- src/VDT.Core.DependencyInjection/Decorators/DecoratorOptions.cs
+++ src/VDT.Core.DependencyInjection/Decorators/DecoratorOptions.cs
@@ -1,5 +1,17 @@
-﻿namespace VDT.Core.DependencyInjection.Decorators {
-    public class DecoratorOptions<TService> where TService : class {
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
+namespace VDT.Core.DependencyInjection.Decorators {
+    public class DecoratorOptions<TService> where TService : class {
+        private readonly List<DecoratorPolicy> decorators = new List<DecoratorPolicy>();
+
+        public void AddDecorator<TDecorator>() where TDecorator : class, IDecorator {
+            AddDecorator<TDecorator>(methodInfo => true);
+        }
+
+        public void AddDecorator<TDecorator>(Func<MethodInfo, bool> shouldInterceptFunc) where TDecorator : class, IDecorator {
+            decorators.Add(new DecoratorPolicy<TDecorator>(shouldInterceptFunc));
+        }
     }
 }
---
+++ src/VDT.Core.DependencyInjection/Decorators/DecoratorPolicy.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+
+namespace VDT.Core.DependencyInjection.Decorators {
+    internal abstract class DecoratorPolicy {
+        private readonly Func<MethodInfo, bool> shouldInterceptFunc;
+
+        internal DecoratorPolicy(Func<MethodInfo, bool> shouldInterceptFunc) {
+            this.shouldInterceptFunc = shouldInterceptFunc;
+        }
+
+        internal bool ShouldIntercept(MethodInfo methodInfo) {
+            return shouldInterceptFunc(methodInfo);
+        }
+
+        internal abstract IDecorator Resolve(IServiceProvider serviceProvider);
+    }
+
+    internal class DecoratorPolicy<TDecorator> : DecoratorPolicy where TDecorator : class, IDecorator {
+        internal DecoratorPolicy(Func<MethodInfo, bool> shouldInterceptFunc) : base(shouldInterceptFunc) { }
+
+        internal override IDecorator Resolve(IServiceProvider serviceProvider) {
+            return serviceProvider.GetRequiredService<TDecorator>();
+        }
+    }
+}

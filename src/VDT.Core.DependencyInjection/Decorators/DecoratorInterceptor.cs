9927b310f9099d61010424fa6dc5a97965cfad27
Maikel Bos <maikel.bos0@gmail.com>
2021-02-24 08:17:47 +0100

    Introduction of interceptor

---
+++ src/VDT.Core.DependencyInjection/Decorators/DecoratorInterceptor.cs
@@ -0,0 +1,16 @@
+﻿using Castle.DynamicProxy;
+using System;
+
+namespace VDT.Core.DependencyInjection.Decorators {
+    internal class DecoratorInterceptor : IInterceptor {
+        private readonly IDecorator decorator;
+
+        public DecoratorInterceptor(IDecorator decorator) {
+            this.decorator = decorator;
+        }
+
+        public void Intercept(IInvocation invocation) {
+            throw new NotImplementedException();
+        }
+    }
+}

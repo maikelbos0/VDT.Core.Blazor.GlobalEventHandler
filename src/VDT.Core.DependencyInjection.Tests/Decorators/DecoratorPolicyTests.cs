ceb5541e03150feb8cfb58546135ae362037c7e5
Maikel Bos <maikel.bos0@gmail.com>
2021-02-27 14:12:48 +0100

    GetDecorator test

---
+++ src/VDT.Core.DependencyInjection.Tests/Decorators/DecoratorPolicyTests.cs
@@ -0,0 +1,19 @@
+﻿using NSubstitute;
+using System;
+using VDT.Core.DependencyInjection.Decorators;
+using Xunit;
+
+namespace VDT.Core.DependencyInjection.Tests.Decorators {
+    public sealed class DecoratorPolicyTests {
+        [Fact]
+        public void GetDecorator_Works() {
+            var policy = new DecoratorPolicy<TestDecorator>(m => true);
+            var serviceProvider = Substitute.For<IServiceProvider>();
+            var decorator = new TestDecorator();
+
+            serviceProvider.GetService(typeof(TestDecorator)).Returns(decorator);
+
+            Assert.Equal(decorator, policy.GetDecorator(serviceProvider));
+        }
+    }
+}

cda9528ab6f8630f88c009173476893b72287a6e
Maikel Bos <maikel.bos0@gmail.com>
2021-02-28 10:21:47 +0100

    First service collection test

---
+++ src/VDT.Core.DependencyInjection.Tests/Decorators/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VDT.Core.DependencyInjection.Decorators;
+using Xunit;
+
+namespace VDT.Core.DependencyInjection.Tests.Decorators {
+    public class ServiceCollectionTarget {
+        public virtual void Execute() {
+        }
+    }
+
+    public sealed class ServiceCollectionExtensionsTests {
+        [Fact]
+        public void AddScoped_Adds_DecoratorInjectors() {
+            var services = new ServiceCollection();
+            var decorator = new TestDecorator();
+
+            services.AddSingleton(decorator);
+            services.AddScoped<ServiceCollectionTarget, ServiceCollectionTarget>(options => {
+                options.AddDecorator<TestDecorator>();
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+            var proxy = serviceProvider.GetRequiredService<ServiceCollectionTarget>();
+
+            proxy.Execute();
+
+            Assert.Equal(1, decorator.Calls);
+        }
+    }
+}
--- src/VDT.Core.DependencyInjection.Tests/Decorators/TestDecorator.cs
+++ src/VDT.Core.DependencyInjection.Tests/Decorators/TestDecorator.cs
@@ -2,5 +2,10 @@
 
 namespace VDT.Core.DependencyInjection.Tests.Decorators {
     public class TestDecorator : IDecorator {
+        public int Calls { get; private set; }
+
+        public void BeforeExecute(MethodExecutionContext context) {
+            Calls++;
+        }
     }
 }

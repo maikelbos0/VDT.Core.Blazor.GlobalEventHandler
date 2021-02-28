661954e2c3d48e1bb30053133c4fb044eeb7a9f8
Maikel Bos <maikel.bos0@gmail.com>
2021-02-28 10:29:16 +0100

    Full test coverage for scoped services

--- src/VDT.Core.DependencyInjection.Tests/Decorators/ServiceCollectionExtensionsTests.cs
+++ src/VDT.Core.DependencyInjection.Tests/Decorators/ServiceCollectionExtensionsTests.cs
@@ -3,18 +3,19 @@ using VDT.Core.DependencyInjection.Decorators;
 using Xunit;
 
 namespace VDT.Core.DependencyInjection.Tests.Decorators {
-    public class ServiceCollectionTarget {
-        public virtual void Execute() {
-        }
-    }
-
     public sealed class ServiceCollectionExtensionsTests {
-        [Fact]
-        public void AddScoped_Adds_DecoratorInjectors() {
-            var services = new ServiceCollection();
-            var decorator = new TestDecorator();
+        private readonly ServiceCollection services;
+        private readonly TestDecorator decorator;
+
+        public ServiceCollectionExtensionsTests() {
+            services = new ServiceCollection();
+            decorator = new TestDecorator();
 
             services.AddSingleton(decorator);
+        }
+
+        [Fact]
+        public void AddScoped_Adds_DecoratorInjectors() {
             services.AddScoped<ServiceCollectionTarget, ServiceCollectionTarget>(options => {
                 options.AddDecorator<TestDecorator>();
             });
@@ -22,7 +23,23 @@ namespace VDT.Core.DependencyInjection.Tests.Decorators {
             var serviceProvider = services.BuildServiceProvider();
             var proxy = serviceProvider.GetRequiredService<ServiceCollectionTarget>();
 
-            proxy.Execute();
+            Assert.Equal("Bar", proxy.GetValue());
+
+            Assert.Equal(1, decorator.Calls);
+        }
+
+        [Fact]
+        public void AddScoped_With_Factory_Adds_DecoratorInjectors() {
+            services.AddScoped<ServiceCollectionTarget, ServiceCollectionTarget>(serviceProvider => new ServiceCollectionTarget {
+                Value = "Foo"
+            }, options => {
+                options.AddDecorator<TestDecorator>();
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+            var proxy = serviceProvider.GetRequiredService<ServiceCollectionTarget>();
+
+            Assert.Equal("Foo", proxy.GetValue());
 
             Assert.Equal(1, decorator.Calls);
         }
---
+++ src/VDT.Core.DependencyInjection.Tests/Decorators/ServiceCollectionTarget.cs
@@ -0,0 +1,9 @@
+﻿namespace VDT.Core.DependencyInjection.Tests.Decorators {
+    public class ServiceCollectionTarget {
+        public string Value { get; set; } = "Bar";
+
+        public virtual string GetValue() {
+            return Value;
+        }
+    }
+}

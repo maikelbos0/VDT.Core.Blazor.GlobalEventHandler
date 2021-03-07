58795683ebfec7128e4c433687ea78b27dbbb9b7
Maikel Bos <maikel.bos0@gmail.com>
2021-03-07 09:19:07 +0100

    Start of attribute based decoration

---
+++ src/VDT.Core.DependencyInjection/Decorators/IDecorateAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace VDT.Core.DependencyInjection.Decorators {
+    /// <summary>
+    /// Provides a mechanism for indicating with attributes which decorators to use on a method
+    /// </summary>
+    /// <typeparam name="TDecorator"></typeparam>
+    public interface IDecorateAttribute<TDecorator> where TDecorator : IDecorator { }
+}

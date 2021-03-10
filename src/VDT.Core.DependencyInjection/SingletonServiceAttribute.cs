3667d0533f7b06bdff87e5f5f4d3bd9f0cb4275d
Maikel Bos <maikel.bos0@gmail.com>
2021-03-10 17:54:09 +0100

    Singleton attribute type registration

--- src/VDT.Core.DependencyInjection/SingletonServiceAttribute.cs
+++ src/VDT.Core.DependencyInjection/SingletonServiceAttribute.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Reflection;
 
 namespace VDT.Core.DependencyInjection {
     /// <summary>
     /// Marks a service to be registered as a singleton service when calling <see cref="ServiceCollectionExtensions.AddAttributeServices"/>
     /// </summary>
     public sealed class SingletonServiceAttribute : ServiceAttribute {
+        private static readonly MethodInfo addServiceMethod = typeof(ServiceCollectionServiceExtensions).GetMethod(nameof(ServiceCollectionServiceExtensions.AddSingleton), 2, BindingFlags.Public | BindingFlags.Static, null, new Type[] { typeof(IServiceCollection) }, null) ?? throw new InvalidOperationException($"Method '{nameof(ServiceCollectionServiceExtensions)}.{nameof(ServiceCollectionServiceExtensions.AddSingleton)}' was not found.");
+        private static readonly MethodInfo addDecoratedServiceMethod = typeof(Decorators.ServiceCollectionExtensions).GetMethod(nameof(Decorators.ServiceCollectionExtensions.AddSingleton), 2, BindingFlags.Public | BindingFlags.Static, null, new Type[] { typeof(IServiceCollection), typeof(Action<Decorators.DecoratorOptions>) }, null) ?? throw new InvalidOperationException($"Method '{nameof(Decorators.ServiceCollectionExtensions)}.{nameof(Decorators.ServiceCollectionExtensions.AddSingleton)}' was not found.");
+
         /// <summary>
         /// Marks a service to be registered as a singleton service when calling <see cref="ServiceCollectionExtensions.AddAttributeServices"/>
         /// </summary>
@@ -14,9 +18,11 @@ namespace VDT.Core.DependencyInjection {
         public SingletonServiceAttribute(Type implementationType) : base(implementationType) { }
 
         internal override void Register(IServiceCollection services, Type serviceType) {
+            addServiceMethod.MakeGenericMethod(serviceType, ImplementationType).Invoke(null, new object[] { services });
         }
 
         internal override void Register(IServiceCollection services, Type serviceType, Action<Decorators.DecoratorOptions> decoratorSetupAction) {
+            addDecoratedServiceMethod.MakeGenericMethod(serviceType, ImplementationType).Invoke(null, new object[] { services, decoratorSetupAction });
         }
     }
 }

72f21e8dfba50e9071e53a0224c7225c29da5bfe
Maikel Bos <maikel.bos0@gmail.com>
2021-03-10 18:52:00 +0100

    Transient registratio nattribute

--- src/VDT.Core.DependencyInjection/TransientServiceAttribute.cs
+++ src/VDT.Core.DependencyInjection/TransientServiceAttribute.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Reflection;
 
 namespace VDT.Core.DependencyInjection {
     /// <summary>
     /// Marks a service to be registered as a transient service when calling <see cref="ServiceCollectionExtensions.AddAttributeServices"/>
     /// </summary>
     public sealed class TransientServiceAttribute : ServiceAttribute {
+        private static readonly MethodInfo addServiceMethod = typeof(ServiceCollectionServiceExtensions).GetMethod(nameof(ServiceCollectionServiceExtensions.AddTransient), 2, BindingFlags.Public | BindingFlags.Static, null, new Type[] { typeof(IServiceCollection) }, null) ?? throw new InvalidOperationException($"Method '{nameof(ServiceCollectionServiceExtensions)}.{nameof(ServiceCollectionServiceExtensions.AddTransient)}' was not found.");
+        private static readonly MethodInfo addDecoratedServiceMethod = typeof(Decorators.ServiceCollectionExtensions).GetMethod(nameof(Decorators.ServiceCollectionExtensions.AddTransient), 2, BindingFlags.Public | BindingFlags.Static, null, new Type[] { typeof(IServiceCollection), typeof(Action<Decorators.DecoratorOptions>) }, null) ?? throw new InvalidOperationException($"Method '{nameof(Decorators.ServiceCollectionExtensions)}.{nameof(Decorators.ServiceCollectionExtensions.AddTransient)}' was not found.");
+
         /// <summary>
         /// Marks a service to be registered as a transient service when calling <see cref="ServiceCollectionExtensions.AddAttributeServices"/>
         /// </summary>
@@ -14,9 +18,11 @@ namespace VDT.Core.DependencyInjection {
         public TransientServiceAttribute(Type implementationType) : base(implementationType) { }
 
         internal override void Register(IServiceCollection services, Type serviceType) {
+            addServiceMethod.MakeGenericMethod(serviceType, ImplementationType).Invoke(null, new object[] { services });
         }
 
         internal override void Register(IServiceCollection services, Type serviceType, Action<Decorators.DecoratorOptions> decoratorSetupAction) {
+            addDecoratedServiceMethod.MakeGenericMethod(serviceType, ImplementationType).Invoke(null, new object[] { services, decoratorSetupAction });
         }
     }
 }

f3b6c594d42862e39502ae1a58e99c31e4a977f4
Maikel Bos <maikel.bos0@gmail.com>
2021-04-11 10:15:31 +0200

    Add interface resolution

--- src/VDT.Core.DependencyInjection/Decorators/DecoratorBinding.cs
+++ src/VDT.Core.DependencyInjection/Decorators/DecoratorBinding.cs
@@ -5,27 +5,34 @@ namespace VDT.Core.DependencyInjection.Decorators {
     internal class DecoratorBinding {
         internal MethodInfo Method { get; }
         internal Type ServiceType { get; }
-        internal MethodInfo ServiceMethod {
-            get {
-                if (Method.DeclaringType == ServiceType) {
-                    return Method;
-                }
-                else if (ServiceType.IsInterface) {
-                    // interface mapping
-                    return null!;
-                }
-                else {
-                    // base class mapping
-                    return null!;
-                }
-            }
-        }
         internal Type DecoratorType { get; }
 
         internal DecoratorBinding(MethodInfo method, Type serviceType, Type decoratorType) {
             Method = method;
             ServiceType = serviceType;
             DecoratorType = decoratorType;
+        }
+
+        internal MethodInfo? GetServiceMethod() {
+            if (Method.DeclaringType == ServiceType) {
+                return Method;
+            }
+            else if (ServiceType.IsInterface) {
+                var map = Method.DeclaringType?.GetInterfaceMap(ServiceType);
+
+                if (map != null) {
+                    var index = Array.IndexOf(map.Value.TargetMethods, Method);
+
+                    if (index > -1) {
+                        return map.Value.InterfaceMethods[index];
+                    }
+                }
+            }
+            else {
+                // base class mapping
+            }
+
+            return null;
         }
     }
 }

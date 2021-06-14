839504b251fe3991f8fe0964f7ebd5bc961c5d9f
Maikel Bos <maikel.bos0@gmail.com>
2021-06-14 16:45:24 +0200

    Add service collection extensions for creating event service

---
+++ src/VDT.Core.Events.Tests/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace VDT.Core.Events.Tests {
+    public sealed class ServiceCollectionExtensionsTests {
+        [Fact]
+        public void AddEventService_Adds_EventService() {
+            var serviceProvider = new ServiceCollection()
+                .AddEventService()
+                .BuildServiceProvider();
+
+            Assert.IsType<EventService>(serviceProvider.GetRequiredService<IEventService>());
+        }
+
+        [Fact]
+        public void AddEventService_Adds_EventService_As_Singleton() {
+            var serviceProvider = new ServiceCollection()
+                .AddEventService()
+                .BuildServiceProvider();
+            IEventService service;
+
+            using (var scope = serviceProvider.CreateScope()) {
+                service = scope.ServiceProvider.GetRequiredService<IEventService>();
+            }
+
+            using (var scope = serviceProvider.CreateScope()) {
+                Assert.Same(service, scope.ServiceProvider.GetRequiredService<IEventService>());
+            }
+        }
+
+        [Fact]
+        public void AddEventService_Adds_EventService_With_ServiceProvider() {
+            var fieldInfo = typeof(EventService).GetField("serviceProvider", BindingFlags.Instance | BindingFlags.NonPublic) ?? throw new InvalidOperationException($"Field '{nameof(EventService)}.serviceProvider' was not found.");
+            var serviceProvider = new ServiceCollection()
+                .AddEventService()
+                .BuildServiceProvider();
+
+            Assert.NotNull(fieldInfo.GetValue(serviceProvider.GetRequiredService<IEventService>()));
+        }
+    }
+}
---
+++ src/VDT.Core.Events/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace VDT.Core.Events {
+    /// <summary>
+    /// Extension methods for adding event services to an <see cref="IServiceCollection"/>
+    /// </summary>
+    public static class ServiceCollectionExtensions {
+        /// <summary>
+        /// Add event services to an <see cref="IServiceCollection"/>
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to</param>
+        /// <returns>A reference to this instance after the operation has completed</returns>
+        public static IServiceCollection AddEventService(this IServiceCollection services) {
+            return services.AddSingleton<IEventService, EventService>(serviceProvider => new EventService(serviceProvider));
+        }
+    }
+}

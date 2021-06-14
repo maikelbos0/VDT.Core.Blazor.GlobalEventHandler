c6bf49d7dc057ef222cfdd794cc153390583428b
Maikel Bos <maikel.bos0@gmail.com>
2021-06-14 09:28:37 +0200

    Set up event handling

---
+++ src/VDT.Core.Events/EventService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VDT.Core.Events {
+    /// <summary>
+    /// Service for registering and dispatching events
+    /// </summary>
+    public class EventService {
+        private readonly Dictionary<Type, List<object>> eventHandlers = new Dictionary<Type, List<object>>();
+
+        /// <summary>
+        /// Register an event handler
+        /// </summary>
+        /// <typeparam name="TEvent">Type of the event to handle</typeparam>
+        /// <param name="handler">Handler that can handle the event</param>
+        /// <remarks>Multiple event handlers can be registered for the same event type</remarks>
+        public void RegisterHandler<TEvent>(IEventHandler<TEvent> handler) {
+            if (!eventHandlers.TryGetValue(typeof(TEvent), out var handlers)) {
+                handlers = new List<object>();
+                eventHandlers.Add(typeof(TEvent), handlers);
+            }
+
+            handlers.Add(handler);
+        }
+
+        /// <summary>
+        /// Dispatch an event and trigger all registered event handlers for that event
+        /// </summary>
+        /// <typeparam name="TEvent">Type of the event to handle</typeparam>
+        /// <param name="event">Event to handle</param>
+        public void Dispatch<TEvent>(TEvent @event) {
+            if (eventHandlers.TryGetValue(typeof(TEvent), out var handlers)) {
+                foreach (var handler in handlers.Cast<IEventHandler<TEvent>>()) {
+                    handler.Handle(@event);
+                }
+            }
+        }
+    }
+}
---
+++ src/VDT.Core.Events/IEventHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace VDT.Core.Events {
+    /// <summary>
+    /// Provides a mechanism for handling events of a certain type
+    /// </summary>
+    /// <typeparam name="TEvent">Type of the event to handle</typeparam>
+    public interface IEventHandler<TEvent> {
+        /// <summary>
+        /// Triggers when an event occurs
+        /// </summary>
+        /// <param name="event">Event to handle</param>
+        void Handle(TEvent @event);
+    }
+}

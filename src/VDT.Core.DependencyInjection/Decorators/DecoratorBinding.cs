e90f3f4c599452a2ed09008ec01794aab65b3cdb
Maikel Bos <maikel.bos0@gmail.com>
2021-06-14 09:16:09 +0200

    Fix some imperfections

--- src/VDT.Core.DependencyInjection/Decorators/DecoratorBinding.cs
+++ src/VDT.Core.DependencyInjection/Decorators/DecoratorBinding.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.Reflection;
 
 namespace VDT.Core.DependencyInjection.Decorators {
-    internal class DecoratorBinding {
+    internal sealed class DecoratorBinding {
         internal MethodInfo Method { get; }
         internal Type ServiceType { get; }
         internal Type DecoratorType { get; }
--- src/VDT.Core.DependencyInjection/Decorators/IDecorateAttribute.cs
+++ src/VDT.Core.DependencyInjection/Decorators/IDecorateAttribute.cs
@@ -1,9 +1,7 @@
 ﻿namespace VDT.Core.DependencyInjection.Decorators {
-    // TODO: allow decorators on implementations
-
     /// <summary>
     /// Provides a mechanism for indicating with attributes which decorators to use on a method
     /// </summary>
-    /// <typeparam name="TDecorator"></typeparam>
+    /// <typeparam name="TDecorator">The type of the decorator to invoke</typeparam>
     public interface IDecorateAttribute<TDecorator> where TDecorator : class, IDecorator { }
 }

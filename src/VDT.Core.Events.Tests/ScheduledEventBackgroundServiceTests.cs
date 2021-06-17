2c130ad357d0d39d6b913592ce32829db357d791
Maikel Bos <maikel.bos0@gmail.com>
2021-06-17 19:41:41 +0200

    Add test for background service

---
+++ src/VDT.Core.Events.Tests/ScheduledEventBackgroundServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using NSubstitute;
+using System.Threading;
+using Xunit;
+
+namespace VDT.Core.Events.Tests {
+    public sealed class ScheduledEventBackgroundServiceTests {
+        [Fact]
+        public void ExecuteAsync_Calls_ScheduledEventService_ExecuteAsync() {
+            var scheduledEventService = Substitute.For<IScheduledEventService>();
+            var service = new ScheduledEventBackgroundService(scheduledEventService);
+            var tokenSource = new CancellationTokenSource();
+
+            service.StartAsync(tokenSource.Token);
+
+            scheduledEventService.Received(1).ExecuteAsync(Arg.Any<CancellationToken>());
+        }
+    }
+}

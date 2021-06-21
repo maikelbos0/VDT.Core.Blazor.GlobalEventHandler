2af9f71f42dedf37bfd47c16dfb4c95b09196511
Maikel Bos <maikel.bos0@gmail.com>
2021-06-21 16:41:41 +0200

    Split tests for clarity

--- src/VDT.Core.Events.Tests/ScheduledEventServiceTests.cs
+++ src/VDT.Core.Events.Tests/ScheduledEventServiceTests.cs
@@ -10,7 +10,34 @@ using Xunit;
 namespace VDT.Core.Events.Tests {
     public sealed class ScheduledEventServiceTests {
         [Fact]
-        public async Task ExecuteAsync_Creates_Task_For_Event_In_Constructor() {
+        public void Constructor_Adds_ScheduledEvent() {
+            var fieldInfo = typeof(ScheduledEventService).GetField("scheduledEvents", BindingFlags.Instance | BindingFlags.NonPublic) ?? throw new InvalidOperationException($"Field '{nameof(ScheduledEventService)}.scheduledEvents' was not found.");
+            var eventService = Substitute.For<IEventService>();
+            var @event = Substitute.For<IScheduledEvent>();
+            var service = new ScheduledEventService(eventService, new[] { @event });
+
+            var scheduledEvents = (IEnumerable<IScheduledEvent>?)fieldInfo.GetValue(service);
+
+            Assert.NotNull(scheduledEvents);
+            Assert.Equal(@event, Assert.Single(scheduledEvents!));
+        }
+
+        [Fact]
+        public void AddScheduledEvent_Adds_ScheduledEvent() {
+            var fieldInfo = typeof(ScheduledEventService).GetField("scheduledEvents", BindingFlags.Instance | BindingFlags.NonPublic) ?? throw new InvalidOperationException($"Field '{nameof(ScheduledEventService)}.scheduledEvents' was not found.");
+            var eventService = Substitute.For<IEventService>();
+            var @event = Substitute.For<IScheduledEvent>();
+            var service = new ScheduledEventService(eventService, new[] { @event });
+
+            var scheduledEvents = (IEnumerable<IScheduledEvent>?)fieldInfo.GetValue(service);
+
+            Assert.NotNull(scheduledEvents);
+            Assert.Equal(@event, Assert.Single(scheduledEvents!));
+        }
+
+
+        [Fact]
+        public async Task ExecuteAsync_Creates_Task_For_ScheduledEvent() {
             var fieldInfo = typeof(ScheduledEventService).GetField("scheduledEventRunners", BindingFlags.Instance | BindingFlags.NonPublic) ?? throw new InvalidOperationException($"Field '{nameof(ScheduledEventService)}.scheduledEventRunners' was not found.");
             var eventService = Substitute.For<IEventService>();
             var @event = Substitute.For<IScheduledEvent>();
@@ -32,30 +59,7 @@ namespace VDT.Core.Events.Tests {
         }
 
         [Fact]
-        public async Task ExecuteAsync_Creates_Task_For_AddScheduledEvent() {
-            var fieldInfo = typeof(ScheduledEventService).GetField("scheduledEventRunners", BindingFlags.Instance | BindingFlags.NonPublic) ?? throw new InvalidOperationException($"Field '{nameof(ScheduledEventService)}.scheduledEventRunners' was not found.");
-            var eventService = Substitute.For<IEventService>();
-            var @event = Substitute.For<IScheduledEvent>();
-            var service = new ScheduledEventService(eventService, Enumerable.Empty<IScheduledEvent>());
-            var tokenSource = new CancellationTokenSource();
-
-            @event.CronExpression.Returns("* * * * *");
-            service.AddScheduledEvent(@event);
-
-            var task = service.ExecuteAsync(tokenSource.Token);
-
-            tokenSource.Cancel();
-
-            await task;
-
-            var scheduledEventRunners = fieldInfo.GetValue(service);
-
-            Assert.NotNull(scheduledEventRunners);
-            Assert.Single((Dictionary<IScheduledEvent, Task>)scheduledEventRunners!);
-        }
-
-        [Fact]
-        public async Task Cancelling_Execute_Stops_Tasks() {
+        public async Task Cancelling_ExecuteAsync_Stops_Tasks() {
             var fieldInfo = typeof(ScheduledEventService).GetField("scheduledEventRunners", BindingFlags.Instance | BindingFlags.NonPublic) ?? throw new InvalidOperationException($"Field '{nameof(ScheduledEventService)}.scheduledEventRunners' was not found.");
             var eventService = Substitute.For<IEventService>();
             var @event = Substitute.For<IScheduledEvent>();

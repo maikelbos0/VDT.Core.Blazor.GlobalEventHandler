8508c37b7c56660d3818c2f3edfc46a6335ba8d4
Maikel Bos <maikel.bos0@gmail.com>
2021-11-01 09:18:02 +0100

    Add keydown event handling

---
+++ src/VDT.Core.GlobalEventHandler/wwwroot/globaleventhandler.js
@@ -0,0 +1,27 @@
+﻿
+let handlers = {};
+
+export function register(dotNetObjectReference) {
+    let keyDownHandler = function (e) {
+        dotNetObjectReference.invokeMethodAsync("OnKeyDown", {
+            altKey: e.altKey,
+            code: e.code,
+            ctrlKey: e.ctrlKey,
+            key: e.key,
+            location: e.location,
+            metaKey: e.metaKey,
+            repeat: e.repeat,
+            shiftKey: e.shiftKey,
+            type: 'keydown'
+        });
+    }
+
+    handlers[dotNetObjectReference] = keyDownHandler;
+
+    window.addEventListener("keydown", keyDownHandler);
+}
+
+export function unregister(dotNetObjectReference) {
+    window.removeEventListener("keydown", handlers[dotNetObjectReference]);
+    delete handlers[dotNetObjectReference];
+}

3f65bc3079f2f5e84d4dcff1cbce6715ecbc62d5
Maikel Bos <maikel.bos0@gmail.com>
2021-11-05 21:38:47 +0100

    Blazor component test for keydown handler

---
+++ src/VDT.Core.GlobalEventHandler.Tests/GlobalEventHandlerTests.cs
@@ -0,0 +1,24 @@
+﻿using Bunit;
+using Microsoft.AspNetCore.Components.Web;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace VDT.Core.GlobalEventHandler.Tests {
+    public class GlobalEventHandlerTests {
+        [Fact]
+        public async Task GlobalEventHandler_InvokeKeyDown_Invokes_OnKeyDown_Handler() {
+            using var context = new TestContext();
+
+            context.JSInterop.SetupModule("./_content/VDT.Core.GlobalEventHandler/globaleventhandler.js").SetupVoid("register", _ => true);
+
+            KeyboardEventArgs expected = new KeyboardEventArgs();
+            KeyboardEventArgs actual = null!;
+
+            var handler = context.RenderComponent<GlobalEventHandler>(parameters => parameters.Add(p => p.OnKeyDown, (args) => actual = args));
+
+            await handler.Instance.InvokeKeyDown(expected);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}

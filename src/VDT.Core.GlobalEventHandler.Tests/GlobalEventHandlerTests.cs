b679c7c0f02529f73fedb86066adb6a81b94c136
Maikel Bos <maikel.bos0@gmail.com>
2021-11-06 09:03:37 +0100

    Blazor component tests for all handlers

--- src/VDT.Core.GlobalEventHandler.Tests/GlobalEventHandlerTests.cs
+++ src/VDT.Core.GlobalEventHandler.Tests/GlobalEventHandlerTests.cs
@@ -20,5 +20,53 @@ namespace VDT.Core.GlobalEventHandler.Tests {
 
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public async Task GlobalEventHandler_InvokeKeyUp_Invokes_OnKeyUp_Handler() {
+            using var context = new TestContext();
+
+            context.JSInterop.SetupModule("./_content/VDT.Core.GlobalEventHandler/globaleventhandler.js").SetupVoid("register", _ => true);
+
+            KeyboardEventArgs expected = new KeyboardEventArgs();
+            KeyboardEventArgs actual = null!;
+
+            var handler = context.RenderComponent<GlobalEventHandler>(parameters => parameters.Add(p => p.OnKeyUp, (args) => actual = args));
+
+            await handler.Instance.InvokeKeyUp(expected);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task GlobalEventHandler_InvokeResize_Invokes_OnResize_Handler() {
+            using var context = new TestContext();
+
+            context.JSInterop.SetupModule("./_content/VDT.Core.GlobalEventHandler/globaleventhandler.js").SetupVoid("register", _ => true);
+
+            ResizeEventArgs expected = new ResizeEventArgs(0, 0);
+            ResizeEventArgs actual = null!;
+
+            var handler = context.RenderComponent<GlobalEventHandler>(parameters => parameters.Add(p => p.OnResize, (args) => actual = args));
+
+            await handler.Instance.InvokeResize(expected);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task GlobalEventHandler_InvokeClick_Invokes_OnClick_Handler() {
+            using var context = new TestContext();
+
+            context.JSInterop.SetupModule("./_content/VDT.Core.GlobalEventHandler/globaleventhandler.js").SetupVoid("register", _ => true);
+
+            MouseEventArgs expected = new MouseEventArgs();
+            MouseEventArgs actual = null!;
+
+            var handler = context.RenderComponent<GlobalEventHandler>(parameters => parameters.Add(p => p.OnClick, (args) => actual = args));
+
+            await handler.Instance.InvokeClick(expected);
+
+            Assert.Equal(expected, actual);
+        }
     }
 }

61136cda565860870df8489424b947352eecca28
Maikel Bos <maikel.bos0@gmail.com>
2021-11-24 19:21:54 +0100

    Remove TODO

--- src/VDT.Core.DependencyInjection/TypeExtensions.cs
+++ src/VDT.Core.DependencyInjection/TypeExtensions.cs
@@ -2,8 +2,6 @@
 using System.Reflection;
 
 namespace VDT.Core.DependencyInjection {
-    // TODO update microsoft dependency injection package
-    // TODO support for net 6.0
     internal static class TypeExtensions {
         internal static MethodInfo GetMethod(this Type type, string name, int genericParameterCount, BindingFlags bindingFlags, params Type[] types) {
             return type.GetMethod(name, genericParameterCount, bindingFlags, null, types, null) ?? throw new InvalidOperationException($"Method '{type.FullName}.{name}' was not found.");

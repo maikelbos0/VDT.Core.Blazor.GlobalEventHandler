b3350b9d9e3727eb10e0ba10da8cce3be6659ea2
Maikel Bos <maikel.bos0@gmail.com>
2021-12-19 09:18:35 +0100

    Add remaining documentation

--- src/VDT.Core.Blazor.GlobalEventHandler/GlobalEventHandler.cs
+++ src/VDT.Core.Blazor.GlobalEventHandler/GlobalEventHandler.cs
@@ -5,6 +5,9 @@ using System;
 using System.Threading.Tasks;
 
 namespace VDT.Core.Blazor.GlobalEventHandler {
+    /// <summary>
+    /// Allows components to subscribe to window-level javascript events
+    /// </summary>
     public class GlobalEventHandler : ComponentBase, IAsyncDisposable {
         internal const string ModuleLocation = "./_content/VDT.Core.Blazor.GlobalEventHandler/globaleventhandler.js";
 
@@ -63,15 +66,74 @@ namespace VDT.Core.Blazor.GlobalEventHandler {
         /// </summary>
         [Parameter] public EventCallback<ScrollEventArgs> OnScroll { get; set; }
 
+        /// <summary>
+        /// Invoke the callback for the key down event
+        /// </summary>
+        /// <param name="args">Keyboard event information</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
         [JSInvokable] public async Task InvokeKeyDown(KeyboardEventArgs args) => await OnKeyDown.InvokeAsync(args);
+
+        /// <summary>
+        /// Invoke the callback for the key up event
+        /// </summary>
+        /// <param name="args">Keyboard event information</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
         [JSInvokable] public async Task InvokeKeyUp(KeyboardEventArgs args) => await OnKeyUp.InvokeAsync(args);
+
+        /// <summary>
+        /// Invoke the callback for the resize event
+        /// </summary>
+        /// <param name="args">Resize event information</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
         [JSInvokable] public async Task InvokeResize(ResizeEventArgs args) => await OnResize.InvokeAsync(args);
+
+        /// <summary>
+        /// Invoke the callback for the click event
+        /// </summary>
+        /// <param name="args">Mouse event information</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
         [JSInvokable] public async Task InvokeClick(MouseEventArgs args) => await OnClick.InvokeAsync(args);
+
+        /// <summary>
+        /// Invoke the callback for the mouse down event
+        /// </summary>
+        /// <param name="args">Mouse event information</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
         [JSInvokable] public async Task InvokeMouseDown(MouseEventArgs args) => await OnMouseDown.InvokeAsync(args);
+
+        /// <summary>
+        /// Invoke the callback for the mouse up event
+        /// </summary>
+        /// <param name="args">Mouse event information</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
         [JSInvokable] public async Task InvokeMouseUp(MouseEventArgs args) => await OnMouseUp.InvokeAsync(args);
+
+        /// <summary>
+        /// Invoke the callback for the mouse move event
+        /// </summary>
+        /// <param name="args">Mouse event information</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
         [JSInvokable] public async Task InvokeMouseMove(MouseEventArgs args) => await OnMouseMove.InvokeAsync(args);
+
+        /// <summary>
+        /// Invoke the callback for the context menu event
+        /// </summary>
+        /// <param name="args">Mouse event information</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
         [JSInvokable] public async Task InvokeContextMenu(MouseEventArgs args) => await OnContextMenu.InvokeAsync(args);
+
+        /// <summary>
+        /// Invoke the callback for the double click event
+        /// </summary>
+        /// <param name="args">Mouse event information</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
         [JSInvokable] public async Task InvokeDoubleClick(MouseEventArgs args) => await OnDoubleClick.InvokeAsync(args);
+
+        /// <summary>
+        /// Invoke the callback for the scroll event
+        /// </summary>
+        /// <param name="args">Scroll event information</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
         [JSInvokable] public async Task InvokeScroll(ScrollEventArgs args) => await OnScroll.InvokeAsync(args);
 
         /// <inheritdoc/>

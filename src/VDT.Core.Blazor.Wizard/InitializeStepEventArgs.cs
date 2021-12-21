7d39be1a0181f9f207fcafdeb24b89799c7ce1b8
Maikel Bos <maikel.bos0@gmail.com>
2021-12-21 19:11:59 +0100

    Add documentation

--- src/VDT.Core.Blazor.Wizard/InitializeStepEventArgs.cs
+++ src/VDT.Core.Blazor.Wizard/InitializeStepEventArgs.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 
 namespace VDT.Core.Blazor.Wizard {
+    /// <summary>
+    /// Supplies information about a wizard step initializing event that is being raised
+    /// </summary>
     public class InitializeStepEventArgs : EventArgs {
+        // TODO pass marker to step somehow?
     }
 }
--- src/VDT.Core.Blazor.Wizard/TryCompleteStepEventArgs.cs
+++ src/VDT.Core.Blazor.Wizard/TryCompleteStepEventArgs.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 
 namespace VDT.Core.Blazor.Wizard {
+    /// <summary>
+    /// Supplies information about a wizard step attempting to complete event that is being raised
+    /// </summary>
     public class TryCompleteStepEventArgs : EventArgs {
+        /// <summary>
+        /// Indicates if completion of the step should be cancelled
+        /// </summary>
         public bool IsCancelled { get; set; }
+
+        // TODO pass marker to step somehow?
     }
 }

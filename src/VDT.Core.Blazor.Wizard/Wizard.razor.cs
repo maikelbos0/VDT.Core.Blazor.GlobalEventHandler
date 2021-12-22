5887af576f2bc117e8b98aba3518977e96b7de15
Maikel Bos <maikel.bos0@gmail.com>
2021-12-22 19:44:51 +0100

    Add documentation

--- src/VDT.Core.Blazor.Wizard/Wizard.razor.cs
+++ src/VDT.Core.Blazor.Wizard/Wizard.razor.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 
 namespace VDT.Core.Blazor.Wizard {
+    /// <summary>
+    /// Component that renders a series of steps to be completed by the user
+    /// </summary>
     public partial class Wizard : ComponentBase {
+        /// <summary>
+        /// Wizard title content
+        /// </summary>
         [Parameter] public RenderFragment? TitleContent { get; set; }
+
+        /// <summary>
+        /// Text that is displayed on the button for the next step
+        /// </summary>
         [Parameter] public string ButtonNextText { get; set; } = "Next";
+
+        /// <summary>
+        /// Text that is displayed on the button for completing the wizard
+        /// </summary>
         [Parameter] public string ButtonFinishText { get; set; } = "Finish";
+
+        /// <summary>
+        /// Content containing the wizard steps in order; any additional content will also be displayed
+        /// </summary>
         [Parameter] public RenderFragment? Steps { get; set; }
 
+        // TODO maybe refactor these into fields/methods
         private List<WizardStep> StepsInternal { get; set; } = new List<WizardStep>();
         private int? ActiveStepIndex { get; set; } = 0;
         internal WizardStep? ActiveStep => ActiveStepIndex.HasValue && StepsInternal.Count > ActiveStepIndex.Value ? StepsInternal[ActiveStepIndex.Value] : null;
+
+        /// <summary>
+        /// Indicates whether or not the wizard is currently active
+        /// </summary>
         public bool IsActive => ActiveStepIndex.HasValue;
 
         internal void AddStep(WizardStep step) {
--- src/VDT.Core.Blazor.Wizard/WizardStep.razor.cs
+++ src/VDT.Core.Blazor.Wizard/WizardStep.razor.cs
@@ -1,15 +1,24 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace VDT.Core.Blazor.Wizard {
+    /// <summary>
+    /// Step as part of a wizard
+    /// </summary>
     public partial class WizardStep : ComponentBase {
-        [CascadingParameter]
-        private Wizard? Parent { get; set; }
+        [CascadingParameter] private Wizard? Parent { get; set; }
 
-        [Parameter]
-        public RenderFragment? ChildContent { get; set; }
+        /// <summary>
+        /// Content of a wizard step such as an explanation or an <see cref="EditForm"/>
+        /// </summary>
+        [Parameter] public RenderFragment? ChildContent { get; set; }
 
+        /// <summary>
+        /// Indicates whether or not the wizard step is currently active
+        /// </summary>
         public bool IsActive => Parent?.ActiveStep == this;
 
+        /// <inheritdoc/>
         protected override void OnInitialized() {
             Parent?.AddStep(this);
         }

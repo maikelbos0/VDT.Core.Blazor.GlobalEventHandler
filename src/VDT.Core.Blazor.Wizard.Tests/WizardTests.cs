fc3dda386f1a19618d14941d881cc4f0567fab62
Maikel Bos <maikel.bos0@gmail.com>
2022-01-08 08:33:22 +0100

    Add wizard stop tests

--- src/VDT.Core.Blazor.Wizard.Tests/WizardTests.cs
+++ src/VDT.Core.Blazor.Wizard.Tests/WizardTests.cs
@@ -7,7 +7,6 @@ namespace VDT.Core.Blazor.Wizard.Tests {
     public class WizardTests {
         /*
          * TODO
-         * Stop
          * AddStep
          * GetSteps
          * GoToPreviousStep
@@ -47,6 +46,37 @@ namespace VDT.Core.Blazor.Wizard.Tests {
             await wizard.Start();
 
             Assert.Equal(2, typeof(Wizard).GetField("activeStepIndex", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(wizard));
+            Assert.Null(arguments);
+        }
+
+        [Fact]
+        public async Task Wizard_Stop_Works() {
+            WizardStoppedEventArgs? arguments = null;
+            var wizard = new Wizard() {
+#pragma warning disable BL0005 // Component parameter should not be set outside of its component.
+                OnStop = EventCallback.Factory.Create<WizardStoppedEventArgs>(this, args => arguments = args)
+#pragma warning restore BL0005 // Component parameter should not be set outside of its component.
+            };
+
+            typeof(Wizard).GetField("activeStepIndex", BindingFlags.NonPublic | BindingFlags.Instance)!.SetValue(wizard, 2);
+
+            await wizard.Stop();
+
+            Assert.Null(typeof(Wizard).GetField("activeStepIndex", BindingFlags.NonPublic | BindingFlags.Instance)!.GetValue(wizard));
+            Assert.NotNull(arguments);
+        }
+
+        [Fact]
+        public async Task Wizard_Stop_Does_Nothing_When_Inactive() {
+            WizardStoppedEventArgs? arguments = null;
+            var wizard = new Wizard() {
+#pragma warning disable BL0005 // Component parameter should not be set outside of its component.
+                OnStop = EventCallback.Factory.Create<WizardStoppedEventArgs>(this, args => arguments = args)
+#pragma warning restore BL0005 // Component parameter should not be set outside of its component.
+            };
+
+            await wizard.Stop();
+
             Assert.Null(arguments);
         }
     }

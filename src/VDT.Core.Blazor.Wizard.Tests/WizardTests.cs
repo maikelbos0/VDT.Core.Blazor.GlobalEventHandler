a30e5d043ebe50297e700389f207079e877622c8
Maikel Bos <maikel.bos0@gmail.com>
2022-01-08 08:52:01 +0100

    Add wizard addstep tests

--- src/VDT.Core.Blazor.Wizard.Tests/WizardTests.cs
+++ src/VDT.Core.Blazor.Wizard.Tests/WizardTests.cs
@@ -7,8 +7,6 @@ namespace VDT.Core.Blazor.Wizard.Tests {
     public class WizardTests {
         /*
          * TODO
-         * AddStep
-         * GetSteps
          * GoToPreviousStep
          * TryCompleteStep
          * BuildRenderTree?
@@ -78,6 +76,66 @@ namespace VDT.Core.Blazor.Wizard.Tests {
             await wizard.Stop();
 
             Assert.Null(arguments);
+        }
+
+        [Fact]
+        public async Task Wizard_AddStep_Works() {
+            var wizard = new Wizard();
+            var step = new WizardStep();
+
+            typeof(Wizard).GetField("activeStepIndex", BindingFlags.NonPublic | BindingFlags.Instance)!.SetValue(wizard, 0);
+
+            await wizard.AddStep(step);
+
+            Assert.Equal(step, Assert.Single(wizard.GetSteps()));
+        }
+
+        [Fact]
+        public async Task Wizard_AddStep_Initializes_First_Step() {
+            WizardStepInitializedEventArgs? arguments = null;
+            var wizard = new Wizard();
+            var step = new WizardStep() {
+#pragma warning disable BL0005 // Component parameter should not be set outside of its component.
+                OnInitialize = EventCallback.Factory.Create<WizardStepInitializedEventArgs>(this, args => arguments = args)
+#pragma warning restore BL0005 // Component parameter should not be set outside of its component.                
+            };
+
+            typeof(Wizard).GetField("activeStepIndex", BindingFlags.NonPublic | BindingFlags.Instance)!.SetValue(wizard, 0);
+
+            await wizard.AddStep(step);
+
+            Assert.NotNull(arguments);
+        }
+
+        [Fact]
+        public async Task Wizard_AddStep_Does_Not_Initialize_Subsequent_Steps() {
+            WizardStepInitializedEventArgs? arguments = null;
+            var wizard = new Wizard();
+            var step = new WizardStep() {
+#pragma warning disable BL0005 // Component parameter should not be set outside of its component.
+                OnInitialize = EventCallback.Factory.Create<WizardStepInitializedEventArgs>(this, args => arguments = args)
+#pragma warning restore BL0005 // Component parameter should not be set outside of its component.                
+            };
+
+            typeof(Wizard).GetField("activeStepIndex", BindingFlags.NonPublic | BindingFlags.Instance)!.SetValue(wizard, 0);
+
+            await wizard.AddStep(new WizardStep());
+            await wizard.AddStep(step);
+
+            Assert.Null(arguments);
+        }
+
+        [Fact]
+        public async Task Wizard_AddStep_Does_Nothing_For_Existing_Step() {
+            var wizard = new Wizard();
+            var step = new WizardStep();
+
+            typeof(Wizard).GetField("activeStepIndex", BindingFlags.NonPublic | BindingFlags.Instance)!.SetValue(wizard, 0);
+
+            await wizard.AddStep(step);
+            await wizard.AddStep(step);
+
+            Assert.Equal(step, Assert.Single(wizard.GetSteps()));
         }
     }
 }

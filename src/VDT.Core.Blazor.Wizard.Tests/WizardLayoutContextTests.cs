35d0118dcc7b9bce39da4467c3fc6a10274faad1
Maikel Bos <maikel.bos0@gmail.com>
2022-01-12 20:51:20 +0100

    Add wizard default layout tests

--- src/VDT.Core.Blazor.Wizard.Tests/WizardLayoutContextTests.cs
+++ src/VDT.Core.Blazor.Wizard.Tests/WizardLayoutContextTests.cs
@@ -48,6 +48,12 @@ namespace VDT.Core.Blazor.Wizard.Tests {
                 Assert.Equal(attributeValue, frames.Array[index].AttributeValue);
             }
 
+            public void AssertElement(int index, string elementName, int innerFrameCount) {
+                Assert.Equal(RenderTreeFrameType.Element, frames.Array[index].FrameType);
+                Assert.Equal(elementName, frames.Array[index].ElementName);
+                Assert.Equal(innerFrameCount, frames.Array[index].ElementSubtreeLength);
+            }
+
             public void AssertContent(int index, string content) {
                 Assert.Equal(RenderTreeFrameType.Text, frames.Array[index].FrameType);
                 Assert.Equal(content, frames.Array[index].TextContent);
@@ -98,6 +104,44 @@ namespace VDT.Core.Blazor.Wizard.Tests {
             context.AssertFrameCount(4);
             context.AssertContent(1, "Step");
             context.AssertContent(3, "Test");
+        }
+
+        [Fact]
+        public void WizardLayoutContext_DefaultLayout_Renders_Correctly() {
+            var wizard = new Wizard() {
+                ActiveStepIndex = 0,
+                ContainerClass = "container",
+                TitleContainerClass = "title-container",
+                TitleContent = builder => builder.AddContent(1, "<h1>Title</h1>"),
+                StepTitleContainerClass = "step-title-container",
+                ButtonContainerClass = "button-container",
+                ContentContainerClass = "content-container"
+            };
+            wizard.StepsInternal.Add(new WizardStep() { 
+                Title = "Step",
+                ChildContent = builder => builder.AddContent(1, "Step content")
+            });
+            var context = TestContext.CreateTestContext(wizard, c => c.DefaultLayout);
+
+            context.AssertFrameCount(29);
+            context.AssertElement(0, "div", 29);
+            context.AssertAttribute(1, "class", "container");
+
+            context.AssertElement(2, "div", 5);
+            context.AssertAttribute(3, "class", "title-container");
+            context.AssertContent(6, "<h1>Title</h1>");
+
+            context.AssertElement(7, "div", 6);
+            context.AssertAttribute(8, "class", "step-title-container");
+            context.AssertContent(12, "Step");
+
+            context.AssertElement(13, "div", 11);
+            context.AssertAttribute(14, "class", "button-container");
+            context.AssertContent(23, "Finish");
+
+            context.AssertElement(24, "div", 5);
+            context.AssertAttribute(25, "class", "content-container");
+            context.AssertContent(28, "Step content");
         }
     }
 }

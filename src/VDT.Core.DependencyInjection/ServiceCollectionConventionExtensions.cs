8338657db04819a66728f76845e9ca452a0a069e
Maikel Bos <maikel.bos0@gmail.com>
2022-01-29 12:14:02 +0100

    Add most generic method for assembly based registration

--- src/VDT.Core.DependencyInjection/ServiceCollectionConventionExtensions.cs
+++ src/VDT.Core.DependencyInjection/ServiceCollectionConventionExtensions.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using VDT.Core.DependencyInjection.Decorators;
 
 namespace VDT.Core.DependencyInjection {
     /// <summary>
@@ -19,8 +18,20 @@ namespace VDT.Core.DependencyInjection {
         /// <param name="serviceFinder">The method that will return service types for any given implementation type</param>
         /// <returns>A reference to this instance after the operation has completed</returns>
         public static IServiceCollection AddTransientServices(this IServiceCollection services, Assembly assembly, Func<Type, IEnumerable<Type>> serviceFinder) {
+            return services.AddServices(assembly, serviceFinder, (serviceType, implementationType) => ServiceLifetime.Transient);
+        }
+
+        /// <summary>
+        /// Provides a mechanism to register all services found in <paramref name="assembly"/> where the <paramref name="serviceFinder"/> finds service types for an implementation type
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/> to add the services to</param>
+        /// <param name="assembly">The <see cref="Assembly"/> in which to look for services</param>
+        /// <param name="serviceFinder">The method that will return service types for any given implementation type</param>
+        /// <param name="lifetimeProvider">The method that will return a <see cref="ServiceLifetime"/> for a given service type and implementation type</param>
+        /// <returns>A reference to this instance after the operation has completed</returns>
+        public static IServiceCollection AddServices(this IServiceCollection services, Assembly assembly, Func<Type, IEnumerable<Type>> serviceFinder, Func<Type, Type, ServiceLifetime> lifetimeProvider) {
             foreach (var context in GetServices(assembly, serviceFinder)) {
-                services.AddTransient(context.ServiceType, context.ImplementationType);
+                services.Add(new ServiceDescriptor(context.ServiceType, context.ImplementationType, lifetimeProvider(context.ServiceType, context.ImplementationType)));
             }
 
             return services;
@@ -33,4 +44,4 @@ namespace VDT.Core.DependencyInjection {
                 .SelectMany(implementationType => serviceFinder(implementationType).Select(serviceType => new ServiceContext(serviceType, implementationType)));
         }
     }
- }
+}

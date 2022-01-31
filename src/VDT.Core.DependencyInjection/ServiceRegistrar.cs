a68ec3bbe82aaead1a50b7d2c4fc50ca0721dec6
Maikel Bos <maikel.bos0@gmail.com>
2022-01-31 10:02:14 +0100

    Add concept of registration options

---
+++ src/VDT.Core.DependencyInjection/ServiceRegistrar.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace VDT.Core.DependencyInjection {
+    /// <summary>
+    /// Signature for methods that register services to an <see cref="IServiceCollection"/>
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to add the services to</param>
+    /// <param name="serviceType">The service type</param>
+    /// <param name="implementationType">The implementation type</param>
+    /// <param name="serviceLifetime">The lifetime of this service</param>
+    /// <returns>A reference to the instance after the operation has completed</returns>
+    public delegate IServiceCollection ServiceRegistrar(IServiceCollection services, Type serviceType, Type implementationType, ServiceLifetime serviceLifetime);
+}
---
+++ src/VDT.Core.DependencyInjection/ServiceRegistrationOptions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+
+namespace VDT.Core.DependencyInjection {
+    /// <summary>
+    /// Options for registering services to a <see cref="IServiceCollection"/>
+    /// </summary>
+    public class ServiceRegistrationOptions {
+        /// <summary>
+        /// Methods that return service types for a given implementation type; service types that appear in any method will be registered
+        /// </summary>
+        public List<ServiceTypeFinder> ServiceTypeFinders { get; set; } = new List<ServiceTypeFinder>();
+
+        /// <summary>
+        /// Methods that returns a service lifetime for a given service and implementation type to be registered
+        /// </summary>
+        public ServiceLifetimeProvider? ServiceLifetimeProvider { get; set; }
+
+        /// <summary>
+        /// Method that register the found services to an <see cref="IServiceCollection"/> with the provided lifetime
+        /// </summary>
+        public ServiceRegistrar? ServiceRegistrar { get; set; }
+    }
+}

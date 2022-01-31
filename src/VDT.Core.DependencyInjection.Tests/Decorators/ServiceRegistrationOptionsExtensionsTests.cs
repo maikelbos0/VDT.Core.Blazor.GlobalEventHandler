79eff02f9d4039964a19de6e2c0f9f17172fb429
Maikel Bos <maikel.bos0@gmail.com>
2022-01-31 10:11:13 +0100

    Add extension method to use decorators in registration options

---
+++ src/VDT.Core.DependencyInjection.Tests/Decorators/ServiceRegistrationOptionsExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VDT.Core.DependencyInjection.Decorators;
+using VDT.Core.DependencyInjection.Tests.Decorators.Targets;
+using Xunit;
+
+namespace VDT.Core.DependencyInjection.Tests.Decorators {
+    public class ServiceRegistrationOptionsExtensionsTests {
+        [Theory]
+        [InlineData(ServiceLifetime.Transient)]
+        [InlineData(ServiceLifetime.Scoped)]
+        [InlineData(ServiceLifetime.Singleton)]
+        public void UseDecoratorServiceRegistrationMethod_Sets_ServiceRegistrationMethod_To_Decorated_AddService_Method(ServiceLifetime serviceLifetime) {
+            var options = new ServiceRegistrationOptions();
+            var services = new ServiceCollection();
+
+            options.UseDecoratorServiceRegistrar(decoratorOptions => { });
+
+            Assert.NotNull(options.ServiceRegistrar);
+
+            options.ServiceRegistrar!(services, typeof(IServiceCollectionTarget), typeof(ServiceCollectionTarget), serviceLifetime);
+
+            Assert.Equal(serviceLifetime, Assert.Single(services, service => service.ServiceType == typeof(IServiceCollectionTarget)).Lifetime);
+            Assert.Equal(serviceLifetime, Assert.Single(services, service => service.ServiceType == typeof(ServiceCollectionTarget)).Lifetime);
+        }
+    }
+}
---
+++ src/VDT.Core.DependencyInjection/Decorators/ServiceRegistrationOptionsExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace VDT.Core.DependencyInjection.Decorators {
+    /// <summary>
+    /// Extension methods for adding decorators to services to be registered using <see cref="ServiceRegistrationOptions"/>
+    /// </summary>
+    public static class ServiceRegistrationOptionsExtensions {
+        private static readonly Dictionary<ServiceLifetime, MethodInfo> decoratedMethods = new Dictionary<ServiceLifetime, MethodInfo>() {
+            { ServiceLifetime.Transient, typeof(ServiceCollectionDecoratorExtensions)
+                .GetMethod(nameof(ServiceCollectionDecoratorExtensions.AddTransient), 2, BindingFlags.Public | BindingFlags.Static, typeof(IServiceCollection), typeof(Action<DecoratorOptions>)) },
+            { ServiceLifetime.Scoped, typeof(ServiceCollectionDecoratorExtensions)
+                .GetMethod(nameof(ServiceCollectionDecoratorExtensions.AddScoped), 2, BindingFlags.Public | BindingFlags.Static, typeof(IServiceCollection), typeof(Action<DecoratorOptions>)) },
+            { ServiceLifetime.Singleton, typeof(ServiceCollectionDecoratorExtensions)
+                .GetMethod(nameof(ServiceCollectionDecoratorExtensions.AddSingleton), 2, BindingFlags.Public | BindingFlags.Static, typeof(IServiceCollection), typeof(Action<DecoratorOptions>)) },
+        };
+
+        /// <summary>
+        /// Use a service registrar that applies decorators to services using the provided setup action
+        /// </summary>
+        /// <param name="options">The options for registering services</param>
+        /// <param name="setupAction">The action that sets up the decorators for this service</param>
+        /// <returns>A reference to this instance after the operation has completed</returns>
+        public static ServiceRegistrationOptions UseDecoratorServiceRegistrar(this ServiceRegistrationOptions options, Action<DecoratorOptions> setupAction) {
+            options.ServiceRegistrar = (services, serviceType, implementationType, serviceLifetime) => {
+                decoratedMethods[serviceLifetime].MakeGenericMethod(serviceType, implementationType).Invoke(null, new object[] { services, setupAction });
+
+                return services;
+            };
+
+            return options;
+        }
+    }
+}

ec54eb290ce6bb6d3216f1758310cb77efc9c123
Maikel Bos <maikel.bos0@gmail.com>
2022-02-02 21:04:22 +0100

    Add work in progress for attribute based registration in options with reminder

---
+++ src/VDT.Core.DependencyInjection.Tests/ServiceRegistrationOptionsAttributeExtensionsTests.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+using Xunit.Sdk;
+
+namespace VDT.Core.DependencyInjection.Tests {
+    public class ServiceRegistrationOptionsAttributeExtensionsTests {
+        [Fact]
+        public void Todo() {
+            throw new XunitException();
+        }
+    }
+}

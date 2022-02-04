3bc808bd49b0ebe49e0908bbfd81e5110cfe53c1
Maikel Bos <maikel.bos0@gmail.com>
2022-02-04 19:08:05 +0100

    Add simple tests for service attribute extensions

---
+++ src/VDT.Core.DependencyInjection.Tests/ServiceCollectionAttributeExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using VDT.Core.DependencyInjection.Tests.AttributeServiceTargets;
+using Xunit;
+
+namespace VDT.Core.DependencyInjection.Tests {
+    public class ServiceCollectionAttributeExtensions {
+        [Fact]
+        public void AddAttributeServices_Adds_Services() {
+            var services = new ServiceCollection();
+
+            services.AddAttributeServices(typeof(ISingletonServiceTarget).Assembly);
+
+            Assert.Single(services, s => s.ServiceType == typeof(ISingletonServiceTarget));
+            Assert.DoesNotContain(services, s => s.ServiceType == typeof(SingletonServiceTarget));
+        }
+
+        [Fact]
+        public void AddAttributeServices_Adds_Services_With_Decorators() {
+            var services = new ServiceCollection();
+
+            services.AddAttributeServices(typeof(ISingletonServiceTarget).Assembly, options => options.AddAttributeDecorators());
+
+            Assert.Single(services, s => s.ServiceType == typeof(ISingletonServiceTarget));
+            Assert.Single(services, s => s.ServiceType == typeof(SingletonServiceTarget));
+        }
+    }
+}

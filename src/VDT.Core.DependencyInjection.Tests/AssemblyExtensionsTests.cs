6c4918ed0fa08a247fbc5c676141a663eb39238e
Maikel Bos <maikel.bos0@gmail.com>
2022-02-08 20:49:47 +0100

    Create assembly finding extension method

---
+++ src/VDT.Core.DependencyInjection.Tests/AssemblyExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace VDT.Core.DependencyInjection.Tests {
+    public class AssemblyExtensionsTests {
+        [Fact]
+        public void GetAssemblies_Finds_All_Assemblies() {
+            var assembly = typeof(AssemblyExtensionsTests).Assembly;
+
+            var foundAssemblies = assembly.GetAssemblies(a => true, a => false);
+
+            Assert.Contains(assembly, foundAssemblies);
+            Assert.Contains(typeof(ServiceRegistrationOptions).Assembly, foundAssemblies);
+            Assert.Contains(typeof(FactAttribute).Assembly, foundAssemblies);
+            Assert.Contains(typeof(string).Assembly, foundAssemblies);
+            Assert.Contains(typeof(System.IO.File).Assembly, foundAssemblies);
+        }
+
+        [Fact]
+        public void GetAssemblies_Finds_Assemblies_Once() {
+            var assembly = typeof(AssemblyExtensionsTests).Assembly;
+
+            var foundAssemblies = assembly.GetAssemblies(a => true, a => false);
+
+            Assert.Single(foundAssemblies, a => a == typeof(Castle.Core.ProxyServices).Assembly);
+        }
+    }
+}
---
+++ src/VDT.Core.DependencyInjection/AssemblyExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace VDT.Core.DependencyInjection {
+    internal static class AssemblyExtensions {
+        public static IEnumerable<Assembly> GetAssemblies(this Assembly rootAssembly, Predicate<Assembly> predicate, Predicate<AssemblyName> endpointPredicate) {
+            var referencedAssemblies = new HashSet<Assembly>() {
+                rootAssembly 
+            };
+            var assembliesToScan = new List<Assembly>() {
+                rootAssembly            
+            };
+
+            for (var i = 0; i < assembliesToScan.Count; i++) {
+                var newAssemblies = assembliesToScan[i]
+                    .GetReferencedAssemblies()
+                    .Where(a => !endpointPredicate(a))
+                    .Select(Assembly.Load)
+                    .Where(a => !referencedAssemblies.Contains(a));
+
+                assembliesToScan.AddRange(newAssemblies);
+                referencedAssemblies.UnionWith(newAssemblies);
+            }
+
+            return referencedAssemblies
+                .Where(a => predicate(a))
+                .ToList();
+        }
+    }
+}

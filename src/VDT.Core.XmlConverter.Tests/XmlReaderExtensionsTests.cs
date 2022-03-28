5d58a4f99accfb6f270fd185342f209fdd7aeb7b
Maikel Bos <maikel.bos0@gmail.com>
2022-03-28 15:49:47 +0200

    Add extension for getting all attributes from xml element

---
+++ src/VDT.Core.XmlConverter.Tests/XmlReaderExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Text;
+using Xunit;
+
+namespace VDT.Core.XmlConverter.Tests {
+    public class XmlReaderExtensionsTests {
+        [Fact]
+        public void GetAttributes_Returns_Element_Attributes_When_Present() {
+            const string xml = "<foo bar=\"bar\" baz=\"quux quux\">Content</foo>";
+
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(xml));
+            using var reader = System.Xml.XmlReader.Create(stream);
+
+            // Move to element
+            reader.Read();
+
+            var attributes = reader.GetAttributes();
+
+            Assert.Equal(2, attributes.Count);
+            Assert.True(attributes.ContainsKey("bar"));
+            Assert.Equal("bar", attributes["bar"]);
+            Assert.True(attributes.ContainsKey("baz"));
+            Assert.Equal("quux quux", attributes["baz"]);
+        }
+
+        [Fact]
+        public void GetAttributes_Returns_Empty_Dictionary_For_Element_Without_Attributes() {
+            const string xml = "<foo>Content</foo>";
+
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(xml));
+            using var reader = System.Xml.XmlReader.Create(stream);
+
+            // Move to element
+            reader.Read();
+
+            Assert.Empty(reader.GetAttributes());
+        }
+
+        [Fact]
+        public void GetAttributes_Returns_Empty_Dictionary_For_Other_NodeType() {
+            const string xml = "<foo bar=\"bar\">Content</foo>";
+
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(xml));
+            using var reader = System.Xml.XmlReader.Create(stream);
+
+            // Move to element
+            reader.Read();
+
+            // Move to content
+            reader.Read();
+
+            Assert.Empty(reader.GetAttributes());
+        }
+    }
+}
---
+++ src/VDT.Core.XmlConverter/XmlReaderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace VDT.Core.XmlConverter {
+    internal static class XmlReaderExtensions {
+        internal static Dictionary<string, string> GetAttributes(this XmlReader reader) {            
+            var attributes = new Dictionary<string, string>();
+
+            for (var i = 0; i < reader.AttributeCount; i++) {
+                reader.MoveToAttribute(i);
+                attributes.Add(reader.Name, reader.Value);
+            }
+
+            reader.MoveToElement();
+
+            return attributes;
+        }
+    }
+}

c61c368764457ca62608e7950fe3dae094406324
Maikel Bos <maikel.bos0@gmail.com>
2022-03-30 09:22:49 +0200

    Add value converter for nodes

---
+++ src/VDT.Core.XmlConverter/Nodes/INodeConverter.cs
@@ -0,0 +1,8 @@
+﻿using System.IO;
+using System.Xml;
+
+namespace VDT.Core.XmlConverter.Nodes {
+    public interface INodeConverter {
+        public void Convert(XmlReader reader, TextWriter writer);
+    }
+}
---
+++ src/VDT.Core.XmlConverter/Nodes/NodeValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Security;
+using System.Xml;
+
+namespace VDT.Core.XmlConverter.Nodes {
+    public class NodeValueConverter : INodeConverter {
+        public bool XmlEncode { get; }
+
+        public NodeValueConverter(bool xmlEncode) {
+            XmlEncode = xmlEncode;
+        }
+
+        public void Convert(XmlReader reader, TextWriter writer) {
+            if (XmlEncode) {
+                writer.Write(SecurityElement.Escape(reader.Value));
+            }
+            else {
+                writer.Write(reader.Value);
+            }
+        }
+    }
+}

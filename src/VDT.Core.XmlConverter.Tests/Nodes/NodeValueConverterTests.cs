10c149dc6d86f69c6b7e8e2931f2a51745ecbd93
Maikel Bos <maikel.bos0@gmail.com>
2022-03-31 09:44:31 +0200

    Add start and end output to node value converter

--- src/VDT.Core.XmlConverter.Tests/Nodes/NodeValueConverterTests.cs
+++ src/VDT.Core.XmlConverter.Tests/Nodes/NodeValueConverterTests.cs
@@ -7,16 +7,18 @@ using Xunit;
 namespace VDT.Core.XmlConverter.Tests.Nodes {
     public class NodeValueConverterTests {
         [Theory]
-        [InlineData("<foo>Content</foo>", false, "Content")]
-        [InlineData("<foo>Content &amp; this</foo>", false, "Content & this")]
-        [InlineData("<foo>Content</foo>", true, "Content")]
-        [InlineData("<foo>Content &amp; this</foo>", true, "Content &amp; this")]
-        public void Convert_Writes_Value_Correctly(string xml, bool xmlEncode, string expectedValue) {
+        [InlineData("<foo>Content</foo>", false, null, null, "Content")]
+        [InlineData("<foo>Content &amp; this</foo>", false, null, null, "Content & this")]
+        [InlineData("<foo>Content</foo>", true, null, null, "Content")]
+        [InlineData("<foo>Content &amp; this</foo>", true, null, null, "Content &amp; this")]
+        [InlineData("<foo>Content &amp; this</foo>", false, "**", "**", "**Content & this**")]
+        [InlineData("<foo>Content &amp; this</foo>", true, "<foo>", "</foo>", "<foo>Content &amp; this</foo>")]
+        public void Convert_Writes_Value_Correctly(string xml, bool xmlEncodeValue, string? startOutput, string? endOutput, string expectedValue) {
             using var writer = new StringWriter();
             using var stream = new MemoryStream(Encoding.UTF8.GetBytes(xml));
             using var reader = XmlReader.Create(stream);
 
-            var converter = new NodeValueConverter(xmlEncode);
+            var converter = new NodeValueConverter(xmlEncodeValue, startOutput, endOutput);
 
             reader.Read(); // Move to element
             reader.Read(); // Move to text
--- src/VDT.Core.XmlConverter/Nodes/NodeValueConverter.cs
+++ src/VDT.Core.XmlConverter/Nodes/NodeValueConverter.cs
@@ -4,19 +4,30 @@ using System.Xml;
 
 namespace VDT.Core.XmlConverter.Nodes {
     public class NodeValueConverter : INodeConverter {
-        public bool XmlEncode { get; }
+        public bool XmlEncodeValue { get; }
+        public string? StartOuput { get; }
+        public string? EndOutput { get; }
 
-        public NodeValueConverter(bool xmlEncode) {
-            XmlEncode = xmlEncode;
+        public NodeValueConverter(bool xmlEncodeValue) : this(xmlEncodeValue, null, null) {
+        }
+
+        public NodeValueConverter(bool xmlEncodeValue, string? startOuput, string? endOutput) {
+            StartOuput = startOuput;
+            EndOutput = endOutput;
+            XmlEncodeValue = xmlEncodeValue;
         }
 
         public void Convert(XmlReader reader, TextWriter writer) {
-            if (XmlEncode) {
+            writer.Write(StartOuput);
+
+            if (XmlEncodeValue) {
                 writer.Write(SecurityElement.Escape(reader.Value));
             }
             else {
                 writer.Write(reader.Value);
             }
+
+            writer.Write(EndOutput);
         }
     }
 }

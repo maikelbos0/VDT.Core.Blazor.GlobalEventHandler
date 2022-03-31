fd1157eac93a7ca69ec3d5903b18285e6808a0de
Maikel Bos <maikel.bos0@gmail.com>
2022-03-31 11:08:33 +0200

    Add tests for default options

---
+++ src/VDT.Core.XmlConverter.Tests/ConverterOptionsTests.cs
@@ -0,0 +1,51 @@
+﻿using VDT.Core.XmlConverter.Elements;
+using VDT.Core.XmlConverter.Nodes;
+using Xunit;
+
+namespace VDT.Core.XmlConverter.Tests {
+    public class ConverterOptionsTests {
+        [Fact]
+        public void ElementConverters() {
+            var options = new ConverterOptions();
+
+            Assert.Empty(options.ElementConverters);
+        }
+        
+        [Fact]
+        public void DefaultElementConverter() { 
+            var options = new ConverterOptions();
+
+            Assert.IsType<NoOpElementConverter>(options.DefaultElementConverter);
+        }
+
+        [Fact]
+        public void TextConverter() { 
+            var options = new ConverterOptions();
+            var converter = Assert.IsType<NodeValueConverter>(options.TextConverter);
+            
+            Assert.True(converter.XmlEncodeValue);
+            Assert.Null(converter.StartOuput);
+            Assert.Null(converter.EndOutput);
+        }
+
+        [Fact]
+        public void CDataConverter() { 
+            var options = new ConverterOptions();
+            var converter = Assert.IsType<NodeValueConverter>(options.CDataConverter);
+
+            Assert.False(converter.XmlEncodeValue);
+            Assert.Equal("<![CDATA[", converter.StartOuput);
+            Assert.Equal("]]>", converter.EndOutput);
+        }
+
+        [Fact]
+        public void CommentConverter() { 
+            var options = new ConverterOptions();
+            var converter = Assert.IsType<NodeValueConverter>(options.CommentConverter);
+
+            Assert.False(converter.XmlEncodeValue);
+            Assert.Equal("<!--", converter.StartOuput);
+            Assert.Equal("-->", converter.EndOutput);
+        }
+    }
+}

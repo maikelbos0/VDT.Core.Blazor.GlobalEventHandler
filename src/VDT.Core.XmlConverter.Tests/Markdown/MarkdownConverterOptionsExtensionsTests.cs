7421b00ea8d286314bdd2ca91abc5c051ec31bef
Maikel Bos <maikel.bos0@gmail.com>
2022-04-04 20:47:08 +0200

    Add first markdown options

---
+++ src/VDT.Core.XmlConverter.Tests/Markdown/MarkdownConverterOptionsExtensionsTests.cs
@@ -0,0 +1,32 @@
+﻿using VDT.Core.XmlConverter.Markdown;
+using Xunit;
+
+namespace VDT.Core.XmlConverter.Tests.Markdown {
+    public class MarkdownConverterOptionsExtensionsTests {
+        [Theory]
+        [InlineData("<h1>Heading 1</h1>", "# Heading 1\r\n\r\n")]
+        [InlineData("<h2>Heading 2</h2>", "## Heading 2\r\n\r\n")]
+        [InlineData("<h3>Heading 3</h3>", "### Heading 3\r\n\r\n")]
+        [InlineData("<h4>Heading 4</h4>", "#### Heading 4\r\n\r\n")]
+        [InlineData("<h5>Heading 5</h5>", "##### Heading 5\r\n\r\n")]
+        [InlineData("<h6>Heading 6</h6>", "###### Heading 6\r\n\r\n")]
+        public void UseMarkdown_Converts_Header(string xml, string expectedMarkdown) {
+            var options = new ConverterOptions().UseMarkdown();
+            var converter = new Converter(options);
+
+            Assert.Equal(expectedMarkdown, converter.Convert(xml));
+        }
+
+        [Theory]
+        [InlineData("<strong>Bold</strong>", "**Bold**")]
+        [InlineData("<b>Bold</b>", "**Bold**")]
+        [InlineData("<em>Italic</em>", "*Italic*")]
+        [InlineData("<i>Italic</i>", "*Italic*")]
+        public void UseMarkdown_Converts_Emphasis(string xml, string expectedMarkdown) {
+            var options = new ConverterOptions().UseMarkdown();
+            var converter = new Converter(options);
+
+            Assert.Equal(expectedMarkdown, converter.Convert(xml));
+        }
+    }
+}
---
+++ src/VDT.Core.XmlConverter/Markdown/MarkdownConverterOptionsExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using VDT.Core.XmlConverter.Elements;
+
+namespace VDT.Core.XmlConverter.Markdown {
+    public static class MarkdownConverterOptionsExtensions {
+        public static ConverterOptions UseMarkdown(this ConverterOptions options) {
+            // TODO linebreak, paragraph, blockquote, figure out escaping, whitespace, lists, etc
+
+            options.ElementConverters.Add(new BasicElementConverter("# ", $"{Environment.NewLine}{Environment.NewLine}", "h1"));
+            options.ElementConverters.Add(new BasicElementConverter("## ", $"{Environment.NewLine}{Environment.NewLine}", "h2"));
+            options.ElementConverters.Add(new BasicElementConverter("### ", $"{Environment.NewLine}{Environment.NewLine}", "h3"));
+            options.ElementConverters.Add(new BasicElementConverter("#### ", $"{Environment.NewLine}{Environment.NewLine}", "h4"));
+            options.ElementConverters.Add(new BasicElementConverter("##### ", $"{Environment.NewLine}{Environment.NewLine}", "h5"));
+            options.ElementConverters.Add(new BasicElementConverter("###### ", $"{Environment.NewLine}{Environment.NewLine}", "h6"));
+
+            options.ElementConverters.Add(new BasicElementConverter("**", "**", "strong", "b"));
+            options.ElementConverters.Add(new BasicElementConverter("*", "*", "em", "i"));
+
+            // TODO handle other node types
+
+            return options;
+        }
+    }
+}

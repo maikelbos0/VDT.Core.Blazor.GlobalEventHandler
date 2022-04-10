12e606e8b518da926f1dd0d41df89178c972d273
Maikel Bos <maikel.bos0@gmail.com>
2022-04-10 10:18:11 +0200

    Add null element converter for removing elements

---
+++ src/VDT.Core.XmlConverter.Tests/Markdown/NullElementConverterTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using VDT.Core.XmlConverter.Markdown;
+using Xunit;
+
+namespace VDT.Core.XmlConverter.Tests.Markdown {
+    public class NullElementConverterTests {
+        [Theory]
+        [InlineData("foo", true)]
+        [InlineData("FOO", true)]
+        [InlineData("bar", true)]
+        [InlineData("baz", false)]
+        public void IsValidFor(string elementName, bool expectedIsValid) {
+            var converter = new NullElementConverter("foo", "bar");
+
+            Assert.Equal(expectedIsValid, converter.IsValidFor(new ElementData(elementName, new Dictionary<string, string>(), false)));
+        }
+
+        [Fact]
+        public void RenderStart_Renders_StartOuput() {
+            using var writer = new StringWriter();
+            var converter = new NullElementConverter("foo", "bar");
+
+            converter.RenderStart(new ElementData("bar", new Dictionary<string, string>(), false), writer);
+
+            Assert.Equal("", writer.ToString());
+        }
+
+        [Fact]
+        public void ShouldRenderContent_Returns_True() {
+            var converter = new NullElementConverter("foo", "bar");
+
+            Assert.True(converter.ShouldRenderContent(new ElementData("bar", new Dictionary<string, string>(), false)));
+        }
+
+        [Fact]
+        public void RenderEnd_Renders_EndOuput() {
+            using var writer = new StringWriter();
+            var converter = new NullElementConverter("foo", "bar");
+
+            converter.RenderEnd(new ElementData("bar", new Dictionary<string, string>(), false), writer);
+
+            Assert.Equal("", writer.ToString());
+        }
+    }
+}
---
+++ src/VDT.Core.XmlConverter/Markdown/NullElementConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+
+namespace VDT.Core.XmlConverter.Markdown {
+    /// <summary>
+    /// Converter for rendering elements as inline Markdown
+    /// </summary>
+    public class NullElementConverter : BaseElementConverter {
+        /// <summary>
+        /// Constructs an instance of an inline Markdown element converter
+        /// </summary>
+        /// <param name="validForElementNames">Element names for which this converter is valid; names are case-insensitive</param>
+        public NullElementConverter(params string[] validForElementNames) : base(validForElementNames) { }
+
+        /// <inheritdoc/>
+        public override void RenderStart(ElementData elementData, TextWriter writer) { }
+
+        /// <inheritdoc/>
+        public override void RenderEnd(ElementData elementData, TextWriter writer) { }
+    }
+}

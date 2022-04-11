4568548dccda1ac59d8b6ce95e3e4d05e84c0301
Maikel Bos <maikel.bos0@gmail.com>
2022-04-11 12:58:53 +0200

    Add proper text converter for Markdown

---
+++ src/VDT.Core.XmlConverter.Tests/Markdown/TextConverterTests.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml;
+using VDT.Core.XmlConverter.Markdown;
+using Xunit;
+
+namespace VDT.Core.XmlConverter.Tests.Markdown {
+    public class TextConverterTests {
+        [Fact]
+        public void Convert() {
+            using var writer = new StringWriter();
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes("\t Test"));
+            using var reader = XmlReader.Create(stream, new XmlReaderSettings() { ConformanceLevel = ConformanceLevel.Fragment });
+
+            var converter = new TextConverter();
+
+            reader.Read(); // Move to text
+
+            converter.Convert(reader, writer);
+
+            Assert.Equal("Test\r\n", writer.ToString());
+        }
+
+        [Theory]
+        [InlineData("Foo \\ bar", "Foo \\\\ bar\r\n")]
+        [InlineData("Foo ` bar", "Foo \\` bar\r\n")]
+        [InlineData("Foo * bar", "Foo \\* bar\r\n")]
+        [InlineData("Foo _ bar", "Foo \\_ bar\r\n")]
+        [InlineData("Foo { bar", "Foo \\{ bar\r\n")]
+        [InlineData("Foo } bar", "Foo \\} bar\r\n")]
+        [InlineData("Foo [ bar", "Foo \\[ bar\r\n")]
+        [InlineData("Foo ] bar", "Foo \\] bar\r\n")]
+        [InlineData("Foo ( bar", "Foo \\( bar\r\n")]
+        [InlineData("Foo ) bar", "Foo \\) bar\r\n")]
+        [InlineData("Foo # bar", "Foo \\# bar\r\n")]
+        [InlineData("Foo + bar", "Foo \\+ bar\r\n")]
+        [InlineData("Foo - bar", "Foo \\- bar\r\n")]
+        [InlineData("Foo . bar", "Foo \\. bar\r\n")]
+        [InlineData("Foo ! bar", "Foo \\! bar\r\n")]
+        [InlineData("Foo | bar", "Foo \\| bar\r\n")]
+        [InlineData("Foo &lt; bar", "Foo &lt; bar\r\n")]
+        [InlineData("Foo &gt; bar", "Foo &gt; bar\r\n")]
+        [InlineData("Foo &amp; bar", "Foo &amp; bar\r\n")]
+        public void Convert_Escapes_Characters(string xml, string expectedText) {
+            using var writer = new StringWriter();
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(xml));
+            using var reader = XmlReader.Create(stream, new XmlReaderSettings() { ConformanceLevel = ConformanceLevel.Fragment });
+
+            var converter = new TextConverter();
+
+            reader.Read(); // Move to text
+
+            converter.Convert(reader, writer);
+
+            Assert.Equal(expectedText, writer.ToString());
+        }
+    }
+}
---
+++ src/VDT.Core.XmlConverter/Markdown/TextConverter.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace VDT.Core.XmlConverter.Markdown {
+    /// <summary>
+    /// Converter for rendering xml text as Markdown text
+    /// </summary>
+    public class TextConverter : INodeConverter {
+        private static Dictionary<char, string> markdownEscapeCharacters = new Dictionary<char, string>() {
+            { '\\', "\\\\" },
+            { '`', "\\`" },
+            { '*', "\\*" },
+            { '_', "\\_" },
+            { '{', "\\{" },
+            { '}', "\\}" },
+            { '[', "\\[" },
+            { ']', "\\]" },
+            { '(', "\\(" },
+            { ')', "\\)" },
+            { '#', "\\#" },
+            { '+', "\\+" },
+            { '-', "\\-" },
+            { '.', "\\." },
+            { '!', "\\!" },
+            { '|', "\\|" },
+            { '<', "&lt;" },
+            { '>', "&gt;" },
+            { '&', "&amp;" },
+            { '\r', "" },
+            { '\n', "" }
+        };
+
+        /// <inheritdoc/>
+        public void Convert(XmlReader reader, TextWriter writer) {
+            var value = reader.Value.Trim();
+            
+            foreach (var c in value) {
+                if (markdownEscapeCharacters.TryGetValue(c, out var str)) {
+                    writer.Write(str);
+                }
+                else {
+                    writer.Write(c);
+                }
+            }
+
+            writer.WriteLine();
+        }
+    }
+}

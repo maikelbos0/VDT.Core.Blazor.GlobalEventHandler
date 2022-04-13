5174ad3957e7c4a9c5f3345a6411b9659939d215
Maikel Bos <maikel.bos0@gmail.com>
2022-04-13 16:40:56 +0200

    Start moving node parsing to conversion data

---
+++ src/VDT.Core.XmlConverter.Tests/ConversionDataTests.cs
@@ -0,0 +1,64 @@
+﻿using System.IO;
+using System.Text;
+using System.Xml;
+using Xunit;
+
+namespace VDT.Core.XmlConverter.Tests {
+    public class ConversionDataTests {
+        [Theory]
+        [InlineData("<foo/>", XmlNodeType.Element)]
+        [InlineData("<!-- Test -->", XmlNodeType.Comment)]
+        [InlineData("<?xml version=\"1.0\" encoding=\"UTF-8\"?>", XmlNodeType.XmlDeclaration)]
+        [InlineData("<?xml-stylesheet type=\"text/xsl\" href=\"style.xsl\"?>", XmlNodeType.ProcessingInstruction)]
+        public void ReadNode_Sets_NodeType(string xml, XmlNodeType expectedNodeType) {
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(xml));
+            using var reader = XmlReader.Create(stream);
+
+            var data = new ConversionData();
+
+            reader.Read(); // Move to node
+
+            data.ReadNode(reader);
+
+            Assert.Equal(expectedNodeType, data.CurrentNodeType);
+        }
+
+        [Theory]
+        [InlineData("<!-- Test -->")]
+        [InlineData("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")]
+        [InlineData("<?xml-stylesheet type=\"text/xsl\" href=\"style.xsl\"?>")]
+        public void ReadNode_Clears_CurrentElement_If_Needed(string xml) {
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(xml));
+            using var reader = XmlReader.Create(stream);
+
+            var data = new ConversionData();
+
+            reader.Read(); // Move to node
+
+            data.ReadNode(reader);
+
+            Assert.Null(data.CurrentElement);
+        }
+
+        [Theory]
+        [InlineData("<foo bar=\"baz\">Content</foo>", "foo", 1, false)]
+        [InlineData("<foo bar=\"baz\"/>", "foo", 1, true)]
+        [InlineData("<foo>Content</foo>", "foo", 0, false)]
+        [InlineData("<foo/>", "foo", 0, true)]
+        public void ReadNode_Sets_ElementData(string xml, string expectedName, int expectedAttributeCount, bool expectedIsSelfClosing) {
+            using var stream = new MemoryStream(Encoding.UTF8.GetBytes(xml));
+            using var reader = XmlReader.Create(stream);
+
+            var data = new ConversionData();
+
+            reader.Read(); // Move to element
+
+            data.ReadNode(reader);
+
+            Assert.NotNull(data.CurrentElement);
+            Assert.Equal(expectedName, data.CurrentElement?.Name);
+            Assert.Equal(expectedAttributeCount, data.CurrentElement?.Attributes.Count);
+            Assert.Equal(expectedIsSelfClosing, data.CurrentElement?.IsSelfClosing);
+        }
+    }
+}
--- src/VDT.Core.XmlConverter/ConversionData.cs
+++ src/VDT.Core.XmlConverter/ConversionData.cs
@@ -1,7 +1,39 @@
 ﻿using System.Collections.Generic;
+using System.Xml;
 
 namespace VDT.Core.XmlConverter {
-    internal class ConversionData {
+    /// <summary>
+    /// Data relating to a conversion that is in progress
+    /// </summary>
+    public class ConversionData {
         internal Stack<ElementData> ElementAncestors { get; set; } = new Stack<ElementData>();
+
+        /// <summary>
+        /// Type of the current node of the <see cref="XmlReader"/> being converted
+        /// </summary>
+        public XmlNodeType CurrentNodeType { get; private set; } = XmlNodeType.None;
+
+        /// <summary>
+        /// Data about the element being converted if the current node is a <see cref="XmlNodeType.Element"/>; otherwise <see langword="null"/>
+        /// </summary>
+        public ElementData? CurrentElement { get; private set; }
+
+        internal void ReadNode(XmlReader reader) {
+            CurrentNodeType = reader.NodeType;
+
+            if (CurrentNodeType == XmlNodeType.Element) {
+                CurrentElement = new ElementData(
+                    reader.Name,
+                    reader.GetAttributes(),
+                    reader.IsEmptyElement,
+                    ElementAncestors.ToArray()
+                );
+            }
+            else {
+                CurrentElement = null;
+            }
+        }
+
+        // public Dictionary<string, object> AdditionalData { get; } = new Dictionary<string, object>();
     }
 }

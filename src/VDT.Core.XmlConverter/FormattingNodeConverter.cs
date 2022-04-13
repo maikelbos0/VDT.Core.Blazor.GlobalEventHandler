88a3633221c20372ec74818257230a21785bb715
Maikel Bos <maikel.bos0@gmail.com>
2022-04-13 19:19:59 +0200

    Add xml documentation to formatting node converter

--- src/VDT.Core.XmlConverter/FormattingNodeConverter.cs
+++ src/VDT.Core.XmlConverter/FormattingNodeConverter.cs
@@ -3,12 +3,33 @@ using System.Security;
 using System.Xml;
 
 namespace VDT.Core.XmlConverter {
+    /// <summary>
+    /// Converter that converts nodes using a formatting method that accepts the node name and value
+    /// </summary>
     public class FormattingNodeConverter : INodeConverter {
+        /// <summary>
+        /// Formatting method for converting nodes
+        /// </summary>
+        /// <param name="name">Name of the current node</param>
+        /// <param name="value">Text value of the current node</param>
+        /// <returns>Formatted value to use as converted value</returns>
         public delegate string NameValueFormatter(string name, string value);
 
+        /// <summary>
+        /// Formatting method to use
+        /// </summary>
         public NameValueFormatter Formatter { get; set; }
+
+        /// <summary>
+        /// <see langword="true"/> of the value should be xml encoded, or <see langword="false"/> if not
+        /// </summary>
         public bool XmlEncodeValue { get; set; }
 
+        /// <summary>
+        /// Construct a formatting node converter
+        /// </summary>
+        /// <param name="formatter">Formatting method to use</param>
+        /// <param name="xmlEncodeValue"><see langword="true"/> of the value should be xml encoded, or <see langword="false"/> if not</param>
         public FormattingNodeConverter(NameValueFormatter formatter, bool xmlEncodeValue) {
             Formatter = formatter;
             XmlEncodeValue = xmlEncodeValue;

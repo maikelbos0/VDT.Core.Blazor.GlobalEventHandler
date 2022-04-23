3644ed106fc34bb66a1892d403250d799385d391
Maikel Bos <maikel.bos0@gmail.com>
2022-04-23 13:52:24 +0200

    Add method for easily getting new line tracker

---
+++ src/VDT.Core.XmlConverter.Tests/Markdown/NodeDataExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using NSubstitute;
+using System.Collections.Generic;
+using VDT.Core.XmlConverter.Markdown;
+using Xunit;
+
+namespace VDT.Core.XmlConverter.Tests.Markdown {
+    public class NodeDataExtensionsTests {
+        [Fact]
+        public void GetTrailingNewLineTracker_Creates_Tracker_If_Needed() {
+            var nodeData = Substitute.For<INodeData>();
+            var additionalData = new Dictionary<string, object?>();
+
+            nodeData.AdditionalData.Returns(additionalData);
+
+            var tracker = nodeData.GetTrailingNewLineTracker();
+
+            Assert.NotNull(tracker);
+            Assert.True(additionalData.ContainsKey(nameof(TrailingNewLineTracker)));
+            Assert.Equal(tracker, additionalData[nameof(TrailingNewLineTracker)]);
+        }
+
+        [Fact]
+        public void GetTrailingNewLineTracker_Returns_Existing_Tracker() {
+            var nodeData = Substitute.For<INodeData>();
+            var additionalData = new Dictionary<string, object?>();
+            var existingTracker = new TrailingNewLineTracker(additionalData);
+
+            additionalData[nameof(TrailingNewLineTracker)] = existingTracker;
+            nodeData.AdditionalData.Returns(additionalData);
+
+            var tracker = nodeData.GetTrailingNewLineTracker();
+
+            Assert.NotNull(tracker);
+            Assert.Equal(tracker, additionalData[nameof(TrailingNewLineTracker)]);
+            Assert.Equal(tracker, existingTracker);
+        }
+    }
+}
---
+++ src/VDT.Core.XmlConverter/Markdown/NodeDataExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace VDT.Core.XmlConverter.Markdown {
+    /// <summary>
+    /// Markdown extension methods for <see cref="INodeData"/>
+    /// </summary>
+    public static class NodeDataExtensions {
+        /// <summary>
+        /// Get the trailing new line tracker for the node data
+        /// </summary>
+        /// <param name="nodeData">Node data to which the tracker belongs</param>
+        /// <returns>Trailing new line tracker for the node data</returns>
+        public static TrailingNewLineTracker GetTrailingNewLineTracker(this INodeData nodeData) {
+            if (!nodeData.AdditionalData.TryGetValue(nameof(TrailingNewLineTracker), out var elementTrackerObj) || !(elementTrackerObj is TrailingNewLineTracker elementTracker)) {
+                elementTracker = new TrailingNewLineTracker(nodeData.AdditionalData);
+                nodeData.AdditionalData[nameof(TrailingNewLineTracker)] = elementTracker;
+            }
+
+            return elementTracker;
+        }
+    }
+}

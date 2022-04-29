fb5ece7ce87d8f037bfba0bb0f9a47c871da4fc1
Maikel Bos <maikel.bos0@gmail.com>
2022-04-29 20:20:18 +0200

    Add converter for pre content

---
+++ src/VDT.Core.XmlConverter/Markdown/PreContentConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace VDT.Core.XmlConverter.Markdown {
+    internal class PreContentConverter : IElementConverter {
+        private const string preName = "pre";
+
+        /// <inheritdoc/>
+        public bool IsValidFor(ElementData elementData) => elementData.Ancestors.Any(e => string.Equals(e.Name, preName, StringComparison.OrdinalIgnoreCase));
+
+        /// <inheritdoc/>
+        public void RenderStart(ElementData elementData, TextWriter writer) { }
+
+        /// <inheritdoc/>
+        public bool ShouldRenderContent(ElementData elementData) => true;
+
+        /// <inheritdoc/>
+        public void RenderEnd(ElementData elementData, TextWriter writer) { }
+    }
+}

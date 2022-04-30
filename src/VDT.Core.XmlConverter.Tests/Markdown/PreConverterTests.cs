8e6e584065a5a681b1f6ddee35708119ddfaf877
Maikel Bos <maikel.bos0@gmail.com>
2022-04-30 12:28:05 +0200

    Add converter for pre tag

---
+++ src/VDT.Core.XmlConverter.Tests/Markdown/PreConverterTests.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using VDT.Core.XmlConverter.Markdown;
+using Xunit;
+
+namespace VDT.Core.XmlConverter.Tests.Markdown {
+    public class PreConverterTests {
+        [Theory]
+        [InlineData("pre", true)]
+        [InlineData("PRE", true)]
+        [InlineData("foo", false)]
+        public void IsValidFor(string elementName, bool expectedIsValid) {
+            var converter = new PreConverter();
+
+            Assert.Equal(expectedIsValid, converter.IsValidFor(ElementDataHelper.Create(elementName)));
+        }
+
+        [Fact]
+        public void RenderStart() {
+            using var writer = new StringWriter();
+            var converter = new PreConverter();
+
+            converter.RenderStart(ElementDataHelper.Create("pre"), writer);
+
+            Assert.Equal("\r\n```\r\n", writer.ToString());
+        }
+
+        [Fact]
+        public void RenderEnd() {
+            using var writer = new StringWriter();
+
+            var converter = new PreConverter();
+            var elementData = ElementDataHelper.Create(
+                "pre",
+                ElementDataHelper.Create("li")
+            );
+
+            converter.RenderEnd(elementData, writer);
+
+            Assert.Equal("\r\n\t```\r\n\r\n", writer.ToString());
+        }
+    }
+}
---
+++ src/VDT.Core.XmlConverter/Markdown/PreConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace VDT.Core.XmlConverter.Markdown {
+    /// <summary>
+    /// Converter for rendering code blocks as Markdown
+    /// </summary>
+    public class PreConverter : BlockElementConverter {
+        /// <summary>
+        /// Construct an instance of a pre converter
+        /// </summary>
+        public PreConverter() : base($"```{Environment.NewLine}", "pre") {
+        }
+
+        /// <inheritdoc/>
+        public override void RenderEnd(ElementData elementData, TextWriter writer) {
+            base.RenderEnd(elementData, writer);
+
+            var tracker = elementData.GetContentTracker();
+
+            tracker.Write(writer, new string('\t', GetAncestorListItemCount(elementData)));
+            tracker.WriteLine(writer, "```");
+            tracker.WriteLine(writer);
+        }
+    }
+}

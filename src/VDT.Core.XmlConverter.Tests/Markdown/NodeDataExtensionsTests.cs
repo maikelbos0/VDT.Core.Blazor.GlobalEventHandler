8f3c65754858b7a91c52c05793508c9c2ef2836f
Maikel Bos <maikel.bos0@gmail.com>
2022-05-12 17:28:18 +0200

    Create new tracker every time based on current node data

--- src/VDT.Core.XmlConverter.Tests/Markdown/NodeDataExtensionsTests.cs
+++ src/VDT.Core.XmlConverter.Tests/Markdown/NodeDataExtensionsTests.cs
@@ -1,39 +1,17 @@
 ﻿using NSubstitute;
 using System.Collections.Generic;
-using System.Linq;
 using VDT.Core.XmlConverter.Markdown;
 using Xunit;
 
 namespace VDT.Core.XmlConverter.Tests.Markdown {
     public class NodeDataExtensionsTests {
         [Fact]
-        public void GetContentTracker_Creates_Tracker_If_Needed() {
+        public void GetContentTracker_Creates_Tracker() {
             var nodeData = Substitute.For<INodeData>();
-            var additionalData = new Dictionary<string, object?>();
-
-            nodeData.AdditionalData.Returns(additionalData);
 
             var tracker = nodeData.GetContentTracker();
 
             Assert.NotNull(tracker);
-            Assert.True(additionalData.ContainsKey(nameof(ContentTracker)));
-            Assert.Equal(tracker, additionalData[nameof(ContentTracker)]);
-        }
-
-        [Fact]
-        public void GetContentTracker_Returns_Existing_Tracker() {
-            var nodeData = Substitute.For<INodeData>();
-            var additionalData = new Dictionary<string, object?>();
-            var existingTracker = new ContentTracker(nodeData);
-
-            additionalData[nameof(ContentTracker)] = existingTracker;
-            nodeData.AdditionalData.Returns(additionalData);
-
-            var tracker = nodeData.GetContentTracker();
-
-            Assert.NotNull(tracker);
-            Assert.Equal(tracker, additionalData[nameof(ContentTracker)]);
-            Assert.Equal(tracker, existingTracker);
         }
 
         [Theory]
--- src/VDT.Core.XmlConverter/Markdown/NodeDataExtensions.cs
+++ src/VDT.Core.XmlConverter/Markdown/NodeDataExtensions.cs
@@ -12,13 +12,8 @@ namespace VDT.Core.XmlConverter.Markdown {
         /// </summary>
         /// <param name="nodeData">Node data to which the tracker belongs</param>
         /// <returns>Content tracker for the node data</returns>
-        public static ContentTracker GetContentTracker(this INodeData nodeData) {
-            if (!nodeData.AdditionalData.TryGetValue(nameof(ContentTracker), out var elementTrackerObj) || !(elementTrackerObj is ContentTracker elementTracker)) {
-                elementTracker = new ContentTracker(nodeData);
-                nodeData.AdditionalData[nameof(ContentTracker)] = elementTracker;
-            }
-
-            return elementTracker;
+        public static ContentTracker GetContentTracker(this INodeData nodeData) {            
+            return new ContentTracker(nodeData);
         }
 
         /// <summary>

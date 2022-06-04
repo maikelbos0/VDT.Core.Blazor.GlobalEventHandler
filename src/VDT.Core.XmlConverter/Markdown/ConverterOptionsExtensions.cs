a811836f6f47d69cc7d1d47cd365ee920ace8a11
Maikel Bos <maikel.bos0@gmail.com>
2022-06-04 20:43:08 +0200

    Add extended syntax option

--- src/VDT.Core.XmlConverter/Markdown/ConverterOptionsExtensions.cs
+++ src/VDT.Core.XmlConverter/Markdown/ConverterOptionsExtensions.cs
@@ -4,15 +4,28 @@
     /// </summary>
     public static class ConverterOptionsExtensions {
         /// <summary>
-        /// Add converters for all markup supported by basic Markdown
+        /// Add converters for converting HTML markup to Markdown
         /// </summary>
         /// <param name="options">The options for converting XML</param>
         /// <param name="unknownElementHandlingMode">Specifies the way to handle elements that can't be converted to Markdown</param>
+        /// <param name="useExtendedSyntax">Add support for extended Markdown syntax</param>
         /// <returns>A reference to this instance after the operation has completed</returns>
-        public static ConverterOptions UseMarkdown(this ConverterOptions options, UnknownElementHandlingMode unknownElementHandlingMode = UnknownElementHandlingMode.None) {
-            return new ConverterOptionsBuilder()
-                .UseUnknownElementHandlingMode(unknownElementHandlingMode)
-                .Build(options, new ConverterOptionsAssembler());
+        public static ConverterOptions UseMarkdown(
+            this ConverterOptions options, 
+            UnknownElementHandlingMode? unknownElementHandlingMode = null, 
+            bool useExtendedSyntax = false
+        ) {
+            var builder = new ConverterOptionsBuilder();
+
+            if (useExtendedSyntax) {
+                builder.AddAllTargets();
+            }
+
+            if (unknownElementHandlingMode != null) {
+                builder.UseUnknownElementHandlingMode(unknownElementHandlingMode.Value);
+            }
+
+            return builder.Build(options, new ConverterOptionsAssembler());
         }
     }
 }

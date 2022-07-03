c2476d0342115307f06d18e968655ad9b8e8d7f1
Maikel Bos <maikel.bos0@gmail.com>
2022-07-03 11:40:53 +0200

    Add definition description converter

---
+++ src/VDT.Core.XmlConverter/Markdown/DefinitionDescriptionConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace VDT.Core.XmlConverter.Markdown {
+    /// <summary>
+    /// Converter for rendering elements as descriptions of definition terms inside a definition list in Markdown
+    /// </summary>
+    public class DefinitionDescriptionConverter : BlockElementConverter {
+        /// <summary>
+        /// Construct an instance of a Markdown definition description converter
+        /// </summary>
+        public DefinitionDescriptionConverter() : base(": ", "dd") { }
+
+        /// <inheritdoc/>
+        public override void RenderStart(ElementData elementData, TextWriter writer) {
+            base.RenderStart(elementData, writer);
+            elementData.GetContentTracker().Prefixes.Push("\t");
+        }
+
+        /// <inheritdoc/>
+        public override void RenderEnd(ElementData elementData, TextWriter writer) {
+            elementData.GetContentTracker().Prefixes.Pop();
+            base.RenderEnd(elementData, writer);
+        }
+    }
+}

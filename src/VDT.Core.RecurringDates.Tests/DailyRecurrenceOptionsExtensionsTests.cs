4743f0a5348d1d749b9ac115354e25518b9442f4
Maikel Bos <maikel.bos0@gmail.com>
2022-10-02 16:20:18 +0200

    Add tests for daily recurrence extensions

---
+++ src/VDT.Core.RecurringDates.Tests/DailyRecurrenceOptionsExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace VDT.Core.RecurringDates.Tests {
+    public class DailyRecurrenceOptionsExtensionsTests {
+        [Fact]
+        public void IncludeWeekends() {
+            var options = new DailyRecurrenceOptions() {
+                IncludingWeekends = false
+            };
+
+            options.IncludeWeekends();
+
+            Assert.True(options.IncludingWeekends);
+        }
+
+        [Fact]
+        public void ExcludeWeekends() {
+            var options = new DailyRecurrenceOptions() {
+                IncludingWeekends = true
+            };
+
+            options.ExcludeWeekends();
+
+            Assert.False(options.IncludingWeekends);
+        }
+    }
+}

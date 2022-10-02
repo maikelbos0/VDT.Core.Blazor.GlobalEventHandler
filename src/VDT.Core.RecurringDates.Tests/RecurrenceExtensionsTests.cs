c878a1cbc16e1871f141ea4dc31b488e429575cf
Maikel Bos <maikel.bos0@gmail.com>
2022-10-02 09:22:14 +0200

    Add tests for recurrence extensions

---
+++ src/VDT.Core.RecurringDates.Tests/RecurrenceExtensionsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xunit;
+
+namespace VDT.Core.RecurringDates.Tests {
+    public class RecurrenceExtensionsTests {
+        [Fact]
+        public void Days() {
+            var recurrence = new Recurrence();
+
+            recurrence.Days(options => options.IncludingWeekends = false);
+
+            Assert.False(Assert.IsType<DailyRecurrenceOptions>(recurrence.Options).IncludingWeekends);
+        }
+
+        [Fact]
+        public void StartsOn() {
+            var recurrence = new Recurrence();
+
+            recurrence.StartsOn(new DateTime(2022, 1, 1));
+
+            Assert.Equal(new DateTime(2022, 1, 1), recurrence.Start);
+        }
+
+        [Fact]
+        public void EndsOn() {
+            var recurrence = new Recurrence();
+
+            recurrence.EndsOn(new DateTime(2022, 12, 31));
+
+            Assert.Equal(new DateTime(2022, 12, 31), recurrence.End);
+        }
+    }
+}

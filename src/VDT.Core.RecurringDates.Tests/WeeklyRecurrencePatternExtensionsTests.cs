04b8500b31271692992d348c153060bf16d16c88
Maikel Bos <maikel.bos0@gmail.com>
2022-10-04 19:16:18 +0200

    Add fluent extension methods for weekdays

---
+++ src/VDT.Core.RecurringDates.Tests/WeeklyRecurrencePatternExtensionsTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace VDT.Core.RecurringDates.Tests {
+    public class WeeklyRecurrencePatternExtensionsTests {
+        [Fact]
+        public void IncludeDays() {
+            var pattern = new WeeklyRecurrencePattern() {
+                Days = new HashSet<DayOfWeek>() {
+                    DayOfWeek.Tuesday,
+                    DayOfWeek.Wednesday,
+                    DayOfWeek.Thursday
+                }
+            };
+
+            Assert.Same(pattern, pattern.IncludeDays(DayOfWeek.Tuesday, DayOfWeek.Friday));
+
+            Assert.Equal(new[] {
+                DayOfWeek.Tuesday,
+                DayOfWeek.Wednesday,
+                DayOfWeek.Thursday,
+                DayOfWeek.Friday
+            }, pattern.Days);
+        }
+
+        [Fact]
+        public void ExcludeDays() {
+            var pattern = new WeeklyRecurrencePattern() {
+                Days = new HashSet<DayOfWeek>() {
+                    DayOfWeek.Tuesday,
+                    DayOfWeek.Wednesday,
+                    DayOfWeek.Thursday
+                }
+            };
+
+            Assert.Same(pattern, pattern.ExcludeDays(DayOfWeek.Tuesday, DayOfWeek.Friday));
+
+            Assert.Equal(new[] {
+                DayOfWeek.Wednesday,
+                DayOfWeek.Thursday
+            }, pattern.Days);
+        }
+    }
+}
---
+++ src/VDT.Core.RecurringDates/WeeklyRecurrencePatternExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VDT.Core.RecurringDates {
+    public static class WeeklyRecurrencePatternExtensions {
+        public static WeeklyRecurrencePattern IncludeDays(this WeeklyRecurrencePattern pattern, params DayOfWeek[] days)
+            => pattern.IncludeDays(days.AsEnumerable());
+        
+        public static WeeklyRecurrencePattern IncludeDays(this WeeklyRecurrencePattern pattern, IEnumerable<DayOfWeek> days) {
+            pattern.Days.UnionWith(days);
+            return pattern;
+        }
+
+        public static WeeklyRecurrencePattern ExcludeDays(this WeeklyRecurrencePattern pattern, params DayOfWeek[] days)
+            => pattern.ExcludeDays(days.AsEnumerable());
+
+        public static WeeklyRecurrencePattern ExcludeDays(this WeeklyRecurrencePattern pattern, IEnumerable<DayOfWeek> days) {
+            pattern.Days.ExceptWith(days);
+            return pattern;
+        }
+    }
+}

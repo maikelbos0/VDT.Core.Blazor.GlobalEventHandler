4bb85908955c1c39f729ff6f4a1f07bba0937f3f
Maikel Bos <maikel.bos0@gmail.com>
2022-10-22 14:26:57 +0200

    Switch back to int

--- src/VDT.Core.RecurringDates.Tests/YearMonthTests.cs
+++ src/VDT.Core.RecurringDates.Tests/YearMonthTests.cs
@@ -11,7 +11,7 @@ namespace VDT.Core.RecurringDates.Tests {
         [InlineData(2, 0, 2)]
         [InlineData(2, 23, 3)]
         [InlineData(2, 24, 4)]
-        public void Get_Year(uint year, uint month, uint expectedYear) {
+        public void Get_Year(int year, int month, int expectedYear) {
             var yearMonth = new YearMonth(year, month);
 
             Assert.Equal(expectedYear, yearMonth.Year);
@@ -21,7 +21,7 @@ namespace VDT.Core.RecurringDates.Tests {
         [InlineData(0, 0, 1)]
         [InlineData(3, 6, 4)]
         [InlineData(3, 11, 4)]
-        public void Set_Year(uint year, uint month, uint newYear) {
+        public void Set_Year(int year, int month, int newYear) {
             var yearMonth = new YearMonth(year, month);
 
             yearMonth.Year = newYear;
@@ -36,7 +36,7 @@ namespace VDT.Core.RecurringDates.Tests {
         [InlineData(1, 11, 11)]
         [InlineData(1, 23, 11)]
         [InlineData(2, 24, 0)]
-        public void Get_Month(uint year, uint month, uint expectedMonth) {
+        public void Get_Month(int year, int month, int expectedMonth) {
             var yearMonth = new YearMonth(year, month);
 
             Assert.Equal(expectedMonth, yearMonth.Month);
@@ -47,7 +47,7 @@ namespace VDT.Core.RecurringDates.Tests {
         [InlineData(3, 1, 11, 3, 11)]
         [InlineData(3, 11, 1, 3, 1)]
         [InlineData(3, 11, 13, 4, 1)]
-        public void Set_Month(uint year, uint month, uint newMonth, uint expectedYear, uint expectedMonth) {
+        public void Set_Month(int year, int month, int newMonth, int expectedYear, int expectedMonth) {
             var yearMonth = new YearMonth(year, month);
 
             yearMonth.Month = newMonth;
@@ -64,6 +64,15 @@ namespace VDT.Core.RecurringDates.Tests {
 
             Assert.Equal(year, yearMonth.Year);
             Assert.Equal(month, yearMonth.Month);
+        }
+
+        [Fact]
+        public void Convert_From_DateTime() {
+            var dateTime = new DateTime(5, 11, 30);
+            YearMonth yearMonth = dateTime;
+
+            Assert.Equal(dateTime.Year, yearMonth.Year);
+            Assert.Equal(dateTime.Month, yearMonth.Month);
         }
     }
 }
--- src/VDT.Core.RecurringDates/YearMonth.cs
+++ src/VDT.Core.RecurringDates/YearMonth.cs
@@ -1,23 +1,27 @@
-﻿namespace VDT.Core.RecurringDates {
-    internal class YearMonth {
-        private uint totalMonths;
+﻿using System;
 
-        public YearMonth(uint year, uint month) {
+namespace VDT.Core.RecurringDates {
+    internal class YearMonth {
+        private int totalMonths;
+
+        public YearMonth(int year, int month) {
             totalMonths = year * 12 + month;
         }
 
-        internal uint Year {
+        internal int Year {
             get => totalMonths / 12;
             set => totalMonths = totalMonths % 12 + value * 12;
         }
-        internal uint Month {
+        internal int Month {
             get => totalMonths % 12;
             set => totalMonths += value - totalMonths % 12;
         }
 
-        public void Deconstruct(out uint year, out uint month) {
+        public void Deconstruct(out int year, out int month) {
             year = Year;
             month = Month;
         }
+
+        public static implicit operator YearMonth(DateTime date) => new YearMonth(date.Year, date.Month);
     }
 }

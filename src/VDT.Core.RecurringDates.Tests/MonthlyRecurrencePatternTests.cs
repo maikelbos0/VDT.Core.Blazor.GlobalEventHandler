8f337a5ee1264c896a4c702de25e4288ed270ee9
Maikel Bos <maikel.bos0@gmail.com>
2022-10-23 20:03:32 +0200

    Add current day in pattern getter

---
+++ src/VDT.Core.RecurringDates.Tests/MonthlyRecurrencePatternTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace VDT.Core.RecurringDates.Tests {
+    public class MonthlyRecurrencePatternTests {
+        [Theory]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, 1, "2022-01-01", "2022-01-01", 0, 0)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, 1, "2022-01-01", "2022-02-02", 0, 1)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, 3, "2022-01-01", "2022-02-15", 1, 14)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, 3, "2022-01-01", "2022-05-15", 1, 14)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, 3, "2022-01-15", "2022-01-15", 0, 14)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, 3, "2022-01-15", "2022-03-14", 2, 13)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, 1, "2022-01-15", "2022-01-15", 0, 0)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, 1, "2022-01-15", "2022-02-16", 0, 1)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, 3, "2022-01-15", "2022-02-16", 1, 1)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, 3, "2022-01-15", "2022-03-14", 1, 27)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, 3, "2022-01-15", "2022-02-14", 0, 30)]
+        public void GetCurrentDayInPattern(RecurrencePatternPeriodHandling periodHandling, int interval, DateTime start, DateTime current, int expectedMonth, int expectedDay) {
+            var pattern = new MonthlyRecurrencePattern(new Recurrence() {
+                Interval = interval,
+                Start = start
+            }) {
+                PeriodHandling = periodHandling
+            };
+
+            var (month, day) = pattern.GetCurrentDayInPattern(current);
+
+            Assert.Equal(expectedMonth, month);
+            Assert.Equal(expectedDay, day);
+        }
+    }
+}
--- src/VDT.Core.RecurringDates/MonthlyRecurrencePattern.cs
+++ src/VDT.Core.RecurringDates/MonthlyRecurrencePattern.cs
@@ -22,5 +22,27 @@ namespace VDT.Core.RecurringDates {
         public DateTime? GetNext(DateTime current) {
             throw new NotImplementedException();
         }
+
+        internal (int Month, int Day) GetCurrentDayInPattern(DateTime current) {
+            switch (PeriodHandling) {
+                case RecurrencePatternPeriodHandling.Calendar:
+                    return (GetDifferenceInMonths(current, recurrence.Start) % recurrence.Interval, current.Day - 1);
+                case RecurrencePatternPeriodHandling.Ongoing:
+                    if (current.Day >= recurrence.Start.Day) {
+                        return (GetDifferenceInMonths(current, recurrence.Start) % recurrence.Interval, current.Day - recurrence.Start.Day + 0);
+                    }
+                    else {
+                        var yearMonth = new YearMonth(current.Year, current.Month - 1);
+                        var previousMonthDays = DateTime.DaysInMonth(yearMonth.Year, yearMonth.Month);
+
+                        return ((GetDifferenceInMonths(current, recurrence.Start) - 1) % recurrence.Interval, current.Day - recurrence.Start.Day + previousMonthDays);
+                    }
+                default:
+                    throw new NotImplementedException($"No implementation found for {nameof(RecurrencePatternPeriodHandling)} '{PeriodHandling}'");
+            }
+        }
+
+        private int GetDifferenceInMonths(DateTime minuend, DateTime subtrahend)
+            => minuend.Year * 12 + minuend.Month - subtrahend.Year * 12 - subtrahend.Month;
     }
 }

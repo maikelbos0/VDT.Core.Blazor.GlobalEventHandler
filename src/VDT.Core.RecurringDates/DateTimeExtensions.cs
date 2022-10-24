1d0caac98ddbb0bae7e7221ac838e59a2039e09b
Maikel Bos <maikel.bos0@gmail.com>
2022-10-24 10:20:50 +0200

    Refactor get current day in pattern for clarity

--- src/VDT.Core.RecurringDates/DateTimeExtensions.cs
+++ src/VDT.Core.RecurringDates/DateTimeExtensions.cs
@@ -2,6 +2,8 @@
 
 namespace VDT.Core.RecurringDates {
     internal static class DateTimeExtensions {
+        internal static int TotalMonths(this DateTime date) => date.Year * 12 + date.Month;
+
         internal static int DaysInMonth(this DateTime date) => DateTime.DaysInMonth(date.Year, date.Month);
     }
 }
--- src/VDT.Core.RecurringDates/MonthlyRecurrencePattern.cs
+++ src/VDT.Core.RecurringDates/MonthlyRecurrencePattern.cs
@@ -23,23 +23,16 @@ namespace VDT.Core.RecurringDates {
             throw new NotImplementedException();
         }
 
-        internal (int Month, int Day) GetCurrentDayInPattern(DateTime current) {
-            switch (PeriodHandling) {
-                case RecurrencePatternPeriodHandling.Calendar:
-                    return (GetDifferenceInMonths(current, recurrence.Start) % recurrence.Interval, current.Day - 1);
-                case RecurrencePatternPeriodHandling.Ongoing:
-                    if (current.Day >= recurrence.Start.Day) {
-                        return (GetDifferenceInMonths(current, recurrence.Start) % recurrence.Interval, current.Day - recurrence.Start.Day + 0);
-                    }
-                    else {
-                        return ((GetDifferenceInMonths(current, recurrence.Start) - 1) % recurrence.Interval, current.Day - recurrence.Start.Day + current.AddMonths(-1).DaysInMonth());
-                    }
-                default:
-                    throw new NotImplementedException($"No implementation found for {nameof(RecurrencePatternPeriodHandling)} '{PeriodHandling}'");
-            }
-        }
-
-        private int GetDifferenceInMonths(DateTime minuend, DateTime subtrahend)
-            => minuend.Year * 12 + minuend.Month - subtrahend.Year * 12 - subtrahend.Month;
+        internal (int Month, int Day) GetCurrentDayInPattern(DateTime current)
+            => PeriodHandling switch {
+                RecurrencePatternPeriodHandling.Calendar
+                    => ((current.TotalMonths() - recurrence.Start.TotalMonths()) % recurrence.Interval, current.Day - 1),
+                RecurrencePatternPeriodHandling.Ongoing when (current.Day < recurrence.Start.Day)
+                    => ((current.TotalMonths() - recurrence.Start.TotalMonths() - 1) % recurrence.Interval, current.Day - recurrence.Start.Day + current.AddMonths(-1).DaysInMonth()),
+                RecurrencePatternPeriodHandling.Ongoing
+                    => ((current.TotalMonths() - recurrence.Start.TotalMonths()) % recurrence.Interval, current.Day - recurrence.Start.Day),
+                _
+                    => throw new NotImplementedException($"No implementation found for {nameof(RecurrencePatternPeriodHandling)} '{PeriodHandling}'")
+            };
     }
 }

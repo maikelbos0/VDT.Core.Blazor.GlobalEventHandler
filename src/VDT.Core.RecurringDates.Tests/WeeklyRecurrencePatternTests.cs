5bcbd95e7f3eb460372d1f034b4c769005c4cb8b
Maikel Bos <maikel.bos0@gmail.com>
2022-10-24 16:27:27 +0200

    Clean up testcases

--- src/VDT.Core.RecurringDates.Tests/WeeklyRecurrencePatternTests.cs
+++ src/VDT.Core.RecurringDates.Tests/WeeklyRecurrencePatternTests.cs
@@ -5,20 +5,21 @@ using Xunit;
 namespace VDT.Core.RecurringDates.Tests {
     public class WeeklyRecurrencePatternTests {
         [Theory]
-        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-10-03", "2022-10-10", "2022-10-18", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
-        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-10-04", "2022-10-10", "2022-10-18", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
-        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-10-05", "2022-10-10", "2022-10-18", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
-        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-10-05", "2022-10-18", "2022-10-18", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
-        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-10-05", "2022-10-19", "2022-10-22", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, "2022-10-03", "2022-01-01", "2022-10-04", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, "2022-09-26", "2022-10-05", "2022-10-08", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, "2022-09-28", "2022-01-01", "2022-10-01", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
-        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Thursday, 2, "2022-09-28", "2022-01-01", "2022-10-08", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
-        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Thursday, 3, "2022-09-21", "2022-01-01", "2022-10-08", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
-        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Sunday, 2, "2022-09-28", "2022-01-01", "2022-10-01", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
-        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Sunday, 2, "2022-09-28", "2022-10-12", "2022-10-15", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
-        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Sunday, 2, "2022-09-28", "2022-10-15", "2022-10-15", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
-        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Sunday, 2, "2022-09-28", "2022-10-16", "2022-10-18", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-10-03", "2022-10-10", "2022-10-18", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-10-05", "2022-10-10", "2022-10-18", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-10-05", "2022-10-19", "2022-10-22", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Thursday, 3, "2022-09-28", "2022-01-01", "2022-10-15", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Sunday, 1, "2022-09-28", "2022-01-01", "2022-10-01", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Sunday, 1, "2022-09-28", "2022-10-12", "2022-10-15", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Sunday, 1, "2022-09-28", "2022-10-15", "2022-10-15", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Sunday, 1, "2022-09-28", "2022-10-16", "2022-10-18", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Sunday, 3, "2022-09-28", "2022-10-16", "2022-10-22", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
         public void GetFirst(RecurrencePatternPeriodHandling periodHandling, DayOfWeek firstDayOfWeek, int interval, DateTime start, DateTime from, DateTime expected, params DayOfWeek[] daysOfWeek) {
             IRecurrencePattern pattern = new WeeklyRecurrencePattern(new Recurrence() {
                 Interval = interval,
@@ -35,9 +36,12 @@ namespace VDT.Core.RecurringDates.Tests {
         [Theory]
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, "2022-09-26", "2022-09-27", "2022-10-01", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, "2022-09-26", "2022-10-01", "2022-10-04", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-09-26", "2022-10-01", "2022-10-11", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Wednesday, 2, "2022-09-30", "2022-10-04", "2022-10-15", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
-        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Monday, 2, "2022-09-26", "2022-10-01", "2022-10-11", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Friday, 1, "2022-09-26", "2022-10-02", "2022-10-04", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+
         [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Friday, 2, "2022-09-26", "2022-10-01", "2022-10-11", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
         [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Friday, 2, "2022-09-28", "2022-10-01", "2022-10-04", DayOfWeek.Tuesday, DayOfWeek.Saturday)]
         public void GetNext(RecurrencePatternPeriodHandling periodHandling, DayOfWeek firstDayOfWeek, int interval, DateTime start, DateTime current, DateTime expected, params DayOfWeek[] daysOfWeek) {
@@ -78,15 +82,17 @@ namespace VDT.Core.RecurringDates.Tests {
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, "2022-09-26", "2022-10-01", 5)]
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-09-26", "2022-10-01", 5)]
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Wednesday, 2, "2022-09-30", "2022-10-04", 6)]
+        
         [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Monday, 2, "2022-09-26", "2022-10-01", 5)]
         [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Friday, 2, "2022-09-26", "2022-10-01", 5)]
         [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Friday, 2, "2022-09-28", "2022-10-01", 3)]
+        
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-10-03", "2022-10-10", 7)]
-        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-10-04", "2022-10-10", 7)]
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-10-05", "2022-10-10", 7)]
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-10-05", "2022-10-18", 1)]
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 2, "2022-10-05", "2022-10-19", 2)]
         [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, "2022-09-26", "2022-10-05", 2)]
+
         [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Sunday, 2, "2022-09-28", "2022-10-12", 0)]
         [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Sunday, 2, "2022-09-28", "2022-10-15", 3)]
         [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Sunday, 2, "2022-09-28", "2022-10-16", 4)]
@@ -103,137 +109,54 @@ namespace VDT.Core.RecurringDates.Tests {
         }
 
         [Theory]
-        [InlineData(0, false, 7)]
-        [InlineData(1, false, 6)]
-        [InlineData(2, false, 5)]
-        [InlineData(3, false, 4)]
-        [InlineData(4, false, 3)]
-        [InlineData(5, false, 2)]
-        [InlineData(6, false, 1)]
-        [InlineData(0, true, 0)]
-        [InlineData(1, true, 6)]
-        [InlineData(2, true, 5)]
-        [InlineData(3, true, 4)]
-        [InlineData(4, true, 3)]
-        [InlineData(5, true, 2)]
-        [InlineData(6, true, 1)]
-        public void GetNextDayInPattern_Single_Interval_Single_Day(int day, bool allowCurrent, int expectedNextDay) {
-            var pattern = new WeeklyRecurrencePattern(new Recurrence() {
-                Interval = 1
-            }) {
-                PeriodHandling = RecurrencePatternPeriodHandling.Calendar,
-                FirstDayOfWeek = DayOfWeek.Monday,
-                DaysOfWeek = new HashSet<DayOfWeek>() { DayOfWeek.Monday }
-            };
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 0, false, 7, DayOfWeek.Monday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 6, false, 1, DayOfWeek.Monday)]
 
-            Assert.Equal(expectedNextDay, pattern.GetNextDayInPattern(day, allowCurrent));
-        }
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 0, true, 0, DayOfWeek.Monday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 1, true, 6, DayOfWeek.Monday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 6, true, 1, DayOfWeek.Monday)]
 
-        [Theory]
-        [InlineData(0, false, 2)]
-        [InlineData(1, false, 1)]
-        [InlineData(2, false, 2)]
-        [InlineData(3, false, 1)]
-        [InlineData(4, false, 1)]
-        [InlineData(5, false, 4)]
-        [InlineData(6, false, 3)]
-        [InlineData(0, true, 2)]
-        [InlineData(1, true, 1)]
-        [InlineData(2, true, 0)]
-        [InlineData(3, true, 1)]
-        [InlineData(4, true, 0)]
-        [InlineData(5, true, 0)]
-        [InlineData(6, true, 3)]
-        public void GetNextDayInPattern_Single_Interval_Multiple_Days(int day, bool allowCurrent, int expectedNextDay) {
-            var pattern = new WeeklyRecurrencePattern(new Recurrence() {
-                Interval = 1
-            }) {
-                PeriodHandling = RecurrencePatternPeriodHandling.Calendar,
-                FirstDayOfWeek = DayOfWeek.Monday,
-                DaysOfWeek = new HashSet<DayOfWeek>() { DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday }
-            };
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 1, false, 1, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 2, false, 2, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 3, false, 1, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 4, false, 1, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 5, false, 4, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
 
-            Assert.Equal(expectedNextDay, pattern.GetNextDayInPattern(day, allowCurrent));
-        }
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 2, true, 0, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 3, true, 1, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 4, true, 0, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 5, true, 0, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Monday, 1, 6, true, 3, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
 
-        [Theory]
-        [InlineData(0, false, 1)]
-        [InlineData(1, false, 1)]
-        [InlineData(2, false, 4)]
-        [InlineData(3, false, 3)]
-        [InlineData(4, false, 2)]
-        [InlineData(5, false, 1)]
-        [InlineData(6, false, 9)]
-        [InlineData(7, false, 8)]
-        [InlineData(8, false, 7)]
-        [InlineData(9, false, 6)]
-        [InlineData(10, false, 5)]
-        [InlineData(11, false, 4)]
-        [InlineData(12, false, 3)]
-        [InlineData(13, false, 2)]
-        [InlineData(0, true, 1)]
-        [InlineData(1, true, 0)]
-        [InlineData(2, true, 0)]
-        [InlineData(3, true, 3)]
-        [InlineData(4, true, 2)]
-        [InlineData(5, true, 1)]
-        [InlineData(6, true, 0)]
-        [InlineData(7, true, 8)]
-        [InlineData(8, true, 7)]
-        [InlineData(9, true, 6)]
-        [InlineData(10, true, 5)]
-        [InlineData(11, true, 4)]
-        [InlineData(12, true, 3)]
-        [InlineData(13, true, 2)]
-        public void GetNextDayInPattern_Double_Interval_Multiple_Days(int day, bool allowCurrent, int expectedNextDay) {
-            var pattern = new WeeklyRecurrencePattern(new Recurrence() {
-                Interval = 2
-            }) {
-                PeriodHandling = RecurrencePatternPeriodHandling.Calendar,
-                FirstDayOfWeek = DayOfWeek.Thursday,
-                DaysOfWeek = new HashSet<DayOfWeek>() { DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday }
-            };
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Thursday, 2, 0, false, 1, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Thursday, 2, 1, false, 1, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Thursday, 2, 2, false, 4, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Thursday, 2, 5, false, 1, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Thursday, 2, 6, false, 9, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
 
-            Assert.Equal(expectedNextDay, pattern.GetNextDayInPattern(day, allowCurrent));
-        }
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Thursday, 2, 1, true, 0, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Thursday, 2, 2, true, 0, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Thursday, 2, 3, true, 3, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Thursday, 2, 6, true, 0, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Calendar, DayOfWeek.Thursday, 2, 7, true, 8, DayOfWeek.Wednesday, DayOfWeek.Friday, DayOfWeek.Saturday)]
 
-        [Theory]
-        [InlineData(0, false, 3)]
-        [InlineData(1, false, 2)]
-        [InlineData(2, false, 1)]
-        [InlineData(3, false, 3)]
-        [InlineData(4, false, 2)]
-        [InlineData(5, false, 1)]
-        [InlineData(6, false, 11)]
-        [InlineData(7, false, 10)]
-        [InlineData(8, false, 9)]
-        [InlineData(9, false, 8)]
-        [InlineData(10, false, 7)]
-        [InlineData(11, false, 6)]
-        [InlineData(12, false, 5)]
-        [InlineData(13, false, 4)]
-        [InlineData(0, true, 3)]
-        [InlineData(1, true, 2)]
-        [InlineData(2, true, 1)]
-        [InlineData(3, true, 0)]
-        [InlineData(4, true, 2)]
-        [InlineData(5, true, 1)]
-        [InlineData(6, true, 0)]
-        [InlineData(7, true, 10)]
-        [InlineData(8, true, 9)]
-        [InlineData(9, true, 8)]
-        [InlineData(10, true, 7)]
-        [InlineData(11, true, 6)]
-        [InlineData(12, true, 5)]
-        [InlineData(13, true, 4)]
-        public void GetNextDayInPattern_Double_Interval_Multiple_Days_Ongoing(int day, bool allowCurrent, int expectedNextDay) {
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Thursday, 2, 2, false, 1, DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Thursday, 2, 3, false, 3, DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Thursday, 2, 5, false, 1, DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Thursday, 2, 6, false, 11, DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Thursday, 2, 3, true, 0, DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Thursday, 2, 4, true, 2, DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Thursday, 2, 6, true, 0, DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        [InlineData(RecurrencePatternPeriodHandling.Ongoing, DayOfWeek.Thursday, 2, 7, true, 10, DayOfWeek.Tuesday, DayOfWeek.Saturday)]
+        public void GetNextDayInPattern(RecurrencePatternPeriodHandling periodHandling, DayOfWeek firstDayOfWeek, int interval, int day, bool allowCurrent, int expectedNextDay, params DayOfWeek[] daysOfWeek) {
             var pattern = new WeeklyRecurrencePattern(new Recurrence() {
                 Start = new DateTime(2022, 9, 28),
-                Interval = 2
+                Interval = interval
             }) {
-                PeriodHandling = RecurrencePatternPeriodHandling.Ongoing,
-                FirstDayOfWeek = DayOfWeek.Thursday,
-                DaysOfWeek = new HashSet<DayOfWeek>() { DayOfWeek.Tuesday, DayOfWeek.Saturday }
+                PeriodHandling = periodHandling,
+                FirstDayOfWeek = firstDayOfWeek,
+                DaysOfWeek = new HashSet<DayOfWeek>(daysOfWeek)
             };
 
             Assert.Equal(expectedNextDay, pattern.GetNextDayInPattern(day, allowCurrent));

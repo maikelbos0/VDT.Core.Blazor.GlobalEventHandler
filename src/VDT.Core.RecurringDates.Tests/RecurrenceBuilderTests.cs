f422e63f38535bf5cfce1ffca58642cd3a9c2517
Maikel Bos <maikel.bos0@gmail.com>
2022-10-31 16:37:24 +0100

    Start of new builder pattern setup

---
+++ src/VDT.Core.RecurringDates.Tests/RecurrenceBuilderTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Xunit;
+
+namespace VDT.Core.RecurringDates.Tests {
+    public class RecurrenceBuilderTests {
+        [Fact]
+        public void StartsOn() {
+            var builder = new RecurrenceBuilder();
+
+            Assert.Same(builder, builder.StartsOn(new DateTime(2022, 1, 1)));
+
+            Assert.Equal(new DateTime(2022, 1, 1), builder.StartDate);
+        }
+
+        [Fact]
+        public void EndsOn() {
+            var builder = new RecurrenceBuilder();
+
+            Assert.Same(builder, builder.EndsOn(new DateTime(2022, 12, 31)));
+
+            Assert.Equal(new DateTime(2022, 12, 31), builder.EndDate);
+        }
+
+        [Fact]
+        public void RepeatsEvery() {
+            var builder = new RecurrenceBuilder();
+
+            Assert.Same(builder, builder.RepeatsEvery(2));
+
+            Assert.Equal(2, builder.Interval);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-100)]
+        public void RepeatsEvery_Throws_For_Invalid_Interval(int interval) {
+            var builder = new RecurrenceBuilder();
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => builder.RepeatsEvery(interval));
+        }
+    }
+}
---
+++ src/VDT.Core.RecurringDates/RecurrenceBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VDT.Core.RecurringDates {
+    public class RecurrenceBuilder {
+        public int Interval { get; private set; } = 1;
+        public DateTime StartDate { get; private set; } = DateTime.MinValue;
+        public DateTime EndDate { get; private set; } = DateTime.MaxValue;
+
+        public RecurrenceBuilder RepeatsEvery(int interval) {
+            if (interval < 1) {
+                throw new ArgumentOutOfRangeException(nameof(interval), interval, $"Expected {nameof(interval)} to be at least 1.");
+            }
+
+            Interval = interval;
+            return this;
+        }
+
+        public RecurrenceBuilder StartsOn(DateTime startDate) {
+            StartDate = startDate.Date;
+            return this;
+        }
+
+        public RecurrenceBuilder EndsOn(DateTime endDate) {
+            EndDate = endDate.Date;
+            return this;
+        }
+    }
+}

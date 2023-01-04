53d6ad3bb28c16e512e0f760d0593923c81e75b8
Maikel Bos <maikel.bos0@gmail.com>
2023-01-04 19:58:07 +0100

    Add the last xml docs

--- src/VDT.Core.RecurringDates/RecurrencePatternBuilderStart.cs
+++ src/VDT.Core.RecurringDates/RecurrencePatternBuilderStart.cs
@@ -1,26 +1,52 @@
 ﻿namespace VDT.Core.RecurringDates {
+    /// <summary>
+    /// Starting point to add recurrence patterns that repeat with the provided interval
+    /// </summary>
     public class RecurrencePatternBuilderStart {
+        /// <summary>
+        /// Builder for date recurrences to which new pattern builders will be added
+        /// </summary>
         public RecurrenceBuilder RecurrenceBuilder { get; }
 
+        /// <summary>
+        /// Interval between occurrences of the pattern to be created
+        /// </summary>
         public int Interval { get; }
 
-        internal RecurrencePatternBuilderStart(RecurrenceBuilder recurrenceBuilder, int interval) {
+        /// <summary>
+        /// Create a starting point to add recurrence patterns that repeat with the provided interval
+        /// </summary>
+        /// <param name="recurrenceBuilder">Builder for date recurrences to which new pattern builders will be added</param>
+        /// <param name="interval">Interval between occurrences of the pattern to be created</param>
+        public RecurrencePatternBuilderStart(RecurrenceBuilder recurrenceBuilder, int interval) {
             RecurrenceBuilder = recurrenceBuilder;
             Interval = Guard.IsPositive(interval);
         }
 
+        /// <summary>
+        /// Adds a pattern to this recurrence to repeat every <see cref="Interval"/> days
+        /// </summary>
+        /// <returns>A builder to configure the new daily pattern</returns>
         public DailyRecurrencePatternBuilder Days() {
             var builder = new DailyRecurrencePatternBuilder(RecurrenceBuilder, Interval);
             RecurrenceBuilder.PatternBuilders.Add(builder);
             return builder;
         }
 
+        /// <summary>
+        /// Adds a pattern to this recurrence to repeat every <see cref="Interval"/> weeks
+        /// </summary>
+        /// <returns>A builder to configure the new weekly pattern</returns>
         public WeeklyRecurrencePatternBuilder Weeks() {
             var builder = new WeeklyRecurrencePatternBuilder(RecurrenceBuilder, Interval);
             RecurrenceBuilder.PatternBuilders.Add(builder);
             return builder;
         }
 
+        /// <summary>
+        /// Adds a pattern to this recurrence to repeat every <see cref="Interval"/> months
+        /// </summary>
+        /// <returns>A builder to configure the new monthly pattern</returns>
         public MonthlyRecurrencePatternBuilder Months() {
             var builder = new MonthlyRecurrencePatternBuilder(RecurrenceBuilder, Interval);
             RecurrenceBuilder.PatternBuilders.Add(builder);

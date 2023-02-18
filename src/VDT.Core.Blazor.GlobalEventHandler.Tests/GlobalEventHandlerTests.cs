f2fbe569699d1afa8c915b07c3f25878c89b6cb3
Maikel Bos <maikel.bos0@gmail.com>
2023-02-18 11:39:10 +0100

    Test results failure

--- src/VDT.Core.Blazor.GlobalEventHandler.Tests/GlobalEventHandlerTests.cs
+++ src/VDT.Core.Blazor.GlobalEventHandler.Tests/GlobalEventHandlerTests.cs
@@ -51,6 +51,7 @@ namespace VDT.Core.Blazor.GlobalEventHandler.Tests {
             await subject.OnAfterRenderAsync(false);
 
             await module.DidNotReceiveWithAnyArgs().InvokeVoidAsync(Arg.Any<string>(), Arg.Any<object[]>());
+            Assert.Fail("");
         }
 
         [Fact]

13c585196e6d8584ab80b8ee61ae643ca004d86b
Maikel Bos <maikel.bos0@gmail.com>
2026-01-16 15:35:10 +0100

    Switch to top-level statement

--- src/VDT.Core.Blazor.GlobalEventHandler.Examples/Program.cs
+++ src/VDT.Core.Blazor.GlobalEventHandler.Examples/Program.cs
@@ -1,13 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using System.Threading.Tasks;
+using VDT.Core.Blazor.GlobalEventHandler.Examples;
 
-namespace VDT.Core.Blazor.GlobalEventHandler.Examples {
-    public class Program {
-        public static async Task Main(string[] args) {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
